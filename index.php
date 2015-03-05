6d1f625a97c3a718f1946e07ee6fd93776dac520
Dom1n1c <sptzdmnc@gmail.com>
2015-03-05 22:12:57 +0100

    Update index.php
    UTF-8 Fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Magic Mirror</title>
+	<title>Smartmirror</title>
 	<style type="text/css">
 		<?php include('css/main.css') ?>
 	</style>
@@ -9,15 +9,18 @@
 		var gitHash = '<?php echo trim(`git rev-parse HEAD`) ?>';
 	</script>
 	<meta name="google" value="notranslate" />
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 
 	<div class="top left"><div class="date small dimmed"></div><div class="time"></div><div class="calendar xxsmall"></div></div>
 	<div class="top right"><div class="windsun small dimmed"></div><div class="temp"></div><div class="forecast small dimmed"></div></div>
-	<div class="center-ver center-hor"><div class="dishwasher light">Vaatwasser is klaar!</div></div>
+	<div class="center-ver center-hor">
+                <div class="openhab light"></div>
+    	</div>
 	<div class="lower-third center-hor"><div class="compliment light"></div></div>
-	<div class="bottom center-hor"><div class="news medium"></div></div>
+	<div class="bottom center-hor"><div class="newshead newsh medium"></div>
+	<div class="news medium newsd"></div></div>
 
 </div>
 
@@ -25,7 +28,6 @@
 <script src="js/jquery.feedToJSON.js"></script>
 <script src="js/ical_parser.js"></script>
 <script src="js/moment-with-langs.min.js"></script>
-<script src="js/config.js"></script>
 <script src="js/rrule.js"></script>
 <script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
 <script src="js/socket.io.min.js"></script>
