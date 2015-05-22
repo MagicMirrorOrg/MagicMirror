129d3c371e0051694f3510ccbbe0ece323595f04
Dominic Spatz <sptzdmnc@gmail.com>
2015-05-22 11:55:23 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,18 +1,10 @@
 <?php
 	
 	// Set the url of the calendar feed.
-	$url = 'http://www.schulferien.org/iCal/Ferien/icals/Ferien_Nordrhein_Westfalen_2015.ics';
-	//Removed private calendar and unnecessary line-feeds
-
-	/*****************************************/
-
-	// Run the helper function with the desired URL and echo the contents.
-	echo get_url($url);
-
-	// Define the helper function that retrieved the data and decodes the content.
-	function get_url($url)
-	{
-	    //user agent is very necessary, otherwise some websites like google.com wont give zipped content
+	$url1 = 'http://www.schulferien.org/iCal/Feiertage/icals/Feiertage_Nordrhein_Westfalen_2015.ics'
+	$url2 = 'http://www.schulferien.org/iCal/Feiertage/icals/Feiertage_Nordrhein_Westfalen_2016.ics'
+	
+	//user agent is very necessary, otherwise some websites like google.com wont give zipped content
 	    $opts = array(
 	        'http'=>array(
 	            'method'=>"GET",
@@ -22,7 +14,16 @@
 	                        "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0 FirePHP/0.4rn"
 	        )
 	    );
-	 
+		
+	// Added Support for multiple Calendars.
+	echo get_url($url1);
+	echo get_url($url2);
+	
+	
+	// Define the helper function that retrieved the data and decodes the content.
+	function get_url($url)
+	{
+	    
 	    $context = stream_context_create($opts);
 	    $content = file_get_contents($url ,false,$context); 
 	     
