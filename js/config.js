ce6c084345b51e1054cf0c249aa43d181de98a0a
Jon Heller <jon@temboinc.com>
2015-10-23 16:33:41 -0400

    revert config to original settings

--- js/config.js
+++ js/config.js
@@ -7,30 +7,30 @@ var config = {
         //change weather params here:
         //units: metric or imperial
         params: {
-            q: 'Philadelphia, Pennsylvania',
-            units: 'imperial',
+            q: 'Baarn,Netherlands',
+            units: 'metric',
             // if you want a different lang for the weather that what is set above, change it here
             lang: 'en',
-            APPID: 'ba8a928b5152be1d4b371b256554abfc'
+            APPID: 'YOUR_FREE_OPENWEATHER_API_KEY'
         }
     },
     compliments: {
         interval: 2000,
         fadeInterval: 4000,
         morning: [
-            'Are you always so stupid or is today a special occasion?',
-            'Don\'t feel bad. A lot of people have no talent!',
-            'I don\'t know what makes you so stupid, but it really works.'
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
         ],
         afternoon: [
-            'How did you get here? Did someone leave your cage open?',
-            'I know you were born ugly, but why did you have a relapse?',
-            'I know you\'re not as stupid as you look. Nobody could be!'
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
         ],
         evening: [
-            'How come you\'re here? I thought the zoo is closed at night!',
-            'I\'d like to kick you in the teeth, but why should I improve your looks?',
-            'If I had a face like yours. I\'d sue my parents!'
+            'Wow, you look hot!',
+            'You look nice!',
+            'Hi, sexy!'
         ]
     },
     news: {
