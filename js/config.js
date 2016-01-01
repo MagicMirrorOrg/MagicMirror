159c672736f221deb1d2f899583622c5c008552d
leadlag <roantree@gmail.com>
2016-01-01 12:02:26 -0500

    Removed Calendar and added new compliments

--- js/config.js
+++ js/config.js
@@ -18,24 +18,32 @@ var config = {
         interval: 30000,
         fadeInterval: 4000,
         morning: [
-            'Good morning, handsome!',
+            'Good morning!',
             'Enjoy your day!',
             'How was your sleep?'
         ],
         afternoon: [
-            'Hello, beauty!',
-            'You look sexy!',
-            'Looking good today!'
+            'You look nice!',
+            'You look terrifc!',
+            'You are not a total idiot!'
         ],
         evening: [
-            'Wow, you look hot!',
-            'You look nice!',
-            'Hi, sexy!'
+            'You have great taste in mirrors!',
+            'Your face makes other people look ugly!',
+            'Dogs, better than people, since forever'
+        ],
+        birthday: [
+            'Happy Birthday!',
+            'Have a great birthday today!',
+            'Yay, your birthday is today!'
+        ],
+        christmas: [
+            'Merry Christmas!',
+            '¡Feliz Navidad!',
+            'Nollaig Shona Duit!',
+			'Happy Christmas!'
         ]
-    },
-    calendar: {
-        maximumEntries: 10,
-        url: "https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M"
+		
     },
     news: {
         feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
