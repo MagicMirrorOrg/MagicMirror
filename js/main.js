9c9b101a5c40e2f3815774bc99568211efbe9be3
leadlag <roantree@gmail.com>
2016-01-01 11:23:41 -0500

    Removed Calendar Functionality

--- js/main.js
+++ js/main.js
@@ -33,23 +33,9 @@ jQuery(document).ready(function($) {
 
     moment.locale(config.lang);
 
-	//connect do Xbee monitor
-	// var socket = io.connect('http://rpi-alarm.local:8082');
-	// socket.on('dishwasher', function (dishwasherReady) {
-	// 	if (dishwasherReady) {
-	// 		$('.dishwasher').fadeIn(2000);
-	// 		$('.lower-third').fadeOut(2000);
-	// 	} else {
-	// 		$('.dishwasher').fadeOut(2000);
-	// 		$('.lower-third').fadeIn(2000);
-	// 	}
-	// });
-
 	version.init();
 
 	time.init();
-
-	calendar.init();
 
 	compliments.init();
 
