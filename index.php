7c79e14567a76d5a670d4f93262b7c7894896922
leadlag <roantree@gmail.com>
2016-01-01 11:26:35 -0500

    Removed Extraneous IO code

--- index.php
+++ index.php
@@ -32,7 +32,6 @@
 <script src="js/time/time.js" type="text/javascript"></script>
 <script src="js/news/news.js" type="text/javascript"></script>
 <script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
-<!-- <script src="js/socket.io.min.js"></script> -->
 
 </body>
 </html>
