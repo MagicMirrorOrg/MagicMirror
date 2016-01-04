a70c4897ec74041c369ba5a23260dc7e42cbd883
leadlag <roantree@gmail.com>
2016-01-04 16:44:43 -0500

    Using a better snow effect

--- js/weather/weather.js
+++ js/weather/weather.js
@@ -25,7 +25,6 @@ var weather = {
 	temperatureLocation: '.temp',
 	windSunLocation: '.windsun',
 	forecastLocation: '.forecast',
-	snowEffectLocation: '.snoweffect',
 	apiVersion: '2.5',
 	apiBase: 'http://api.openweathermap.org/data/',
 	weatherEndpoint: 'weather',
@@ -118,25 +117,15 @@ weather.updateCurrentWeather = function () {
 				_windDirection = 'N';
 			} 
 			
-			var _snow = '';
-
-			if(data.weather[0].icon == '13d' || data.weather[0].icon == '13n'){
-				_snow = '<div id="snow"></div>';
-			}
-
 			var _newTempHtml = _icon + '' + _temperature + '&deg;';
 
 			$(this.temperatureLocation).updateWithText(_newTempHtml, this.fadeInterval);
-			
-			$(this.snowEffectLocation).updateWithText(_snow, this.fadeInterval);
 
 			var _now = moment().format('HH:mm'),
 				_sunrise = moment(data.sys.sunrise*1000).format('HH:mm') + 'AM',
 				_sunset = moment(data.sys.sunset*1000).format('HH:mm');
 				_sunset12 = moment(data.sys.sunset*1000).format('hh:mm') + 'PM';
 				
-			
-
 			var _newWindHtml = '<span class="wi wi-strong-wind xdimmed"></span> ' + _wind + 'mph ' + _windDirection, //this.ms2Beaufort(_wind),
 				_newSunHtml = '<span class="wi wi-sunrise xdimmed"></span> ' + _sunrise;
 
@@ -197,6 +186,116 @@ weather.updateWeatherForecast = function () {
 		}
 	});
 
+}
+
+window.onload = function(){
+	//canvas init
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	
+	//canvas dimensions
+	var W = window.innerWidth;
+	var H = window.innerHeight;
+	canvas.width = W;
+	canvas.height = H;
+	
+	$.ajax({
+		type: 'GET',
+		url: weather.apiBase + '/' + weather.apiVersion + '/' + weather.weatherEndpoint,
+		dataType: 'json',
+		data: weather.params,
+		success: function (data) {
+
+			_snowEffectActive = 'N';
+
+			if(data.weather[0].icon == '02d' || data.weather[0].icon == '03d'){
+				_snowEffectActive = 'Y';
+			}
+
+		}.bind(this),
+		error: function () {
+
+		}
+	});
+	
+	//snowflake particles
+	var mp = 45; //max particles
+	var particles = [];
+	for(var i = 0; i < mp; i++)
+	{
+		particles.push({
+			x: Math.random()*W, //x-coordinate
+			y: Math.random()*H, //y-coordinate
+			r: Math.random()*4+1, //radius
+			d: Math.random()*mp //density
+		})
+	}
+	
+	//Lets draw the flakes
+	function draw()
+	{
+		ctx.clearRect(0, 0, W, H);
+		
+		if(_snowEffectActive == 'Y'){
+			ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+		} else {
+			ctx.fillStyle = "rgba(255, 255, 255, 0)";
+		}
+		ctx.beginPath();
+		for(var i = 0; i < mp; i++)
+		{
+			var p = particles[i];
+			ctx.moveTo(p.x, p.y);
+			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+		}
+		ctx.fill();
+		update();
+	}
+	
+	//Function to move the snowflakes
+	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+	var angle = 0;
+	function update()
+	{
+		angle += 0.01;
+		for(var i = 0; i < mp; i++)
+		{
+			var p = particles[i];
+			//Updating X and Y coordinates
+			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+			//Every particle has its own density which can be used to make the downward movement different for each flake
+			//Lets make it more random by adding in the radius
+			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+			p.x += Math.sin(angle) * 2;
+			
+			//Sending flakes back from the top when it exits
+			//Lets make it a bit more organic and let flakes enter from the left and right also.
+			if(p.x > W+5 || p.x < -5 || p.y > H)
+			{
+				if(i%3 > 0) //66.67% of the flakes
+				{
+					particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
+				}
+				else
+				{
+					//If the flake is exitting from the right
+					if(Math.sin(angle) > 0)
+					{
+						//Enter from the left
+						particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+					}
+					else
+					{
+						//Enter from the right
+						particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+					}
+				}
+			}
+		}
+	}
+	
+	//animation loop
+	setInterval(draw, 33);
 }
 
 weather.init = function () {
