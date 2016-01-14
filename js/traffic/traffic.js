e206efa7778eeed09ebe7504b6f5b3bb36b152fc
leadlag <roantree@gmail.com>
2016-01-14 18:10:37 -0500

    Only polls Google maps for traffic data during a specified time of day

--- js/traffic/traffic.js
+++ js/traffic/traffic.js
@@ -15,49 +15,49 @@ traffic.updateCurrentTraffic = function () {
 
 	var hour = moment().hour();
 	var dayOfWeek = moment().day();
+	
+	//Only displays traffic in the mornings of weekdays
+	if(hour >= 0 && hour <= 23 && dayOfWeek >= 1 && dayOfWeek <= 5){	
 
 	$.ajax({
-		type: 'GET',
-		url: traffic.apiBase + traffic.apiType,
-		dataType: 'json',
-		data: traffic.params,
-		success: function (data) {
-
-			var _duration = data.routes[0].legs[0].duration.value,
-				_durationInTraffic = data.routes[0].legs[0].duration_in_traffic.value,
-				_durationInTrafficMinutes = data.routes[0].legs[0].duration_in_traffic.text,
-				_trafficTime = _durationInTraffic - _duration,
-				_trafficPhrase = 'No traffic on the way to work';
-
-			//Convert _trafficTime from seconds to minutes
-			_trafficTime = _trafficTime / 60;
-
-			//Verbal traffic conditions
-			if(_trafficTime <= 2){
-				_trafficPhrase = 'No traffic on the way to work';
-			} else if (_trafficTime > 2 && _trafficTime <= 5){
-				_trafficPhrase = 'Light traffic on the way to work';
-			} else if (_trafficTime > 5 && _trafficTime <= 10){
-				_trafficPhrase = 'Moderate traffic on the way to work';
-			} else if (_trafficTime > 10 && _trafficTime <= 15){
-				_trafficPhrase = 'Heavy minute delay going to work';
-			} else {
-				_trafficPhrase = 'Severe traffic going to work';
-			}
+			type: 'GET',
+			url: traffic.apiBase + traffic.apiType,
+			dataType: 'json',
+			data: traffic.params,
+			success: function (data) {
+	
+				var _duration = data.routes[0].legs[0].duration.value,
+					_durationInTraffic = data.routes[0].legs[0].duration_in_traffic.value,
+					_durationInTrafficMinutes = data.routes[0].legs[0].duration_in_traffic.text,
+					_trafficTime = _durationInTraffic - _duration,
+					_trafficPhrase = 'No traffic on the way to work';
+	
+				//Convert _trafficTime from seconds to minutes
+				_trafficTime = _trafficTime / 60;
+	
+				//Verbal traffic conditions
+				if(_trafficTime <= 2){
+					_trafficPhrase = 'No traffic on the way to work';
+				} else if (_trafficTime > 2 && _trafficTime <= 5){
+					_trafficPhrase = 'Light traffic on the way to work';
+				} else if (_trafficTime > 5 && _trafficTime <= 10){
+					_trafficPhrase = 'Moderate traffic on the way to work';
+				} else if (_trafficTime > 10 && _trafficTime <= 15){
+					_trafficPhrase = 'Heavy minute delay going to work';
+				} else {
+					_trafficPhrase = 'Severe traffic going to work';
+				}
 				
-			//Only displays traffic in the mornings of weekdays
-			if(hour >= 7 && hour <= 9 && dayOfWeek >= 1 && dayOfWeek <= 5){		
 				$(this.trafficLocation).updateWithText(_trafficPhrase + '<br> Expected commute is ' + _durationInTrafficMinutes, this.fadeInterval);				
-			} else{
-				$(this.trafficLocation).updateWithText('', this.fadeInterval);
-			}
-			
-		}.bind(this),
-		error: function () {
-		
-		}
-	});
 
+			}.bind(this),
+			error: function () {
+			}
+		});
+	
+	} else{
+		$(this.trafficLocation).updateWithText('', this.fadeInterval);
+	}
 }
 
 traffic.init = function () {
