fb6aef79a749fa3a9158d2a51c8686795a26c378
leadlag <roantree@gmail.com>
2016-01-19 20:26:29 -0500

    Added new compliments

--- js/config.js
+++ js/config.js
@@ -4,21 +4,45 @@ var config = {
         timeFormat: 12
     },
     compliments: {
-        interval: 30000,
+        interval: 60000,
         fadeInterval: 4000,
         morning: [
             'Good morning!',
             'Enjoy your day!',
-            'How was your sleep?'
+            'You look great today!',
+			'I like your style!',
+			'I like your socks!',
+			'Hello, good-looking!',
+			'I like your face!',
+			'I dig your look!',
+			'You light up the room!'
+			
         ],
         afternoon: [
             'You look nice!',
             'You look terrifc!',
-            'You are not a total idiot!'
+            "You're not a total idiot!",
+			'Well done!',
+			'Nice fine motor control!',
+			'Your skin is radiant!',
+			'Killer boots, man!',
+			'You look symmetrical!',
+			"You're sweeter than High Fructose Corn Syrup!",
+			'Nice job on that thing you did that time!',
+			'That color is perfect on you!'
+			
         ],
         evening: [
             'You have great taste in mirrors!',
             'Your face makes other people look ugly!',
+			'Your hair looks stunning!',
+			'You could survive a Zombie apocalypse. Maybe.',
+			'Reading IS fundamental!',
+			'Dogs love you! Some of them. Some dogs are dumb though, so...',
+			'The Force is strong with you',
+			'I was talking to the toilet. He said you have a nice butt.',
+			'Sweet dreams!',
+			'Sleep well!',
             'Dogs, better than people, since forever'
         ],
         birthday: [
