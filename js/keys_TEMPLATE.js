91dcac4bc1a1da2eb9059828872633afb8690ef0
leadlag <roantree@gmail.com>
2016-01-24 15:40:25 -0500

    Added completion information

--- js/keys_TEMPLATE.js
+++ js/keys_TEMPLATE.js
@@ -4,23 +4,28 @@ var keys = {
         //units: metric or imperial
         params: {
             id: 'YOUR_CITY_ID',
+            // Search for your city at http://openweathermap.org/find, and the City ID is the number at the end of the URL you're directed to
             units: 'imperial',
             // if you want a different lang for the weather that what is set above, change it here
             lang: 'en',
             APPID: 'YOUR_OPENWEATHER_API_KEY'
+            // To get your OpenWeather API key, go to http://www.openweathermap.org/news/post/get-api-key-access-weather-api/
         }
     },
 	traffic: {
 		params: {
 			origin: 'place_id:YOUR_STARTING_PLACE_ID',
 			destination: 'place_id:YOUR_ENDING_PLACE_ID',
+            // Use the PlaceID Finder: https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
 			departure_time: 'now',
 			key: 'YOUR_GOOGLE_MAPS_API_KEY'
+            // Go here to get your Google Maps API key: https://developers.google.com/maps/documentation/directions/
 		}
 	},
 	calendar: {
         maximumEntries: 10,
         url: "CALENDAR.ics ADDRESS"
+        // For Google Calendar, it's the 'Private Address' under Calendar Settings when viewing your Calendar
     },
 	birthdays: [
 		{
