5be0d1c5e9e4d3ef025e0eefc9a5b99295840c67
Jonathan Vogt <code@v-o-g-t.net>
2016-01-25 23:57:28 +0100

    Improved Code

--- js/time/time.js
+++ js/time/time.js
@@ -10,27 +10,26 @@ var time = {
  * Updates the time that is shown on the screen
  */
 time.updateTime = function () {
-
-	var _now = moment(),
-		_date = _now.format('[<span class="dayname">]dddd,[</span><span class="longdate">]LL[</span>]');
+	var timeLocation = this.timeLocation;
+	var _now = moment();
+	var _date = _now.format('[<span class="dayname">]dddd,[</span> <span class="longdate">]LL[</span>]');
 	
-
 	$(this.dateLocation).updateWithText(_date, 1000);
 	$('.fade').removeClass('fade')
-	$('#timebuffer').html(_now.format('HH:mm').replace(/./g, "<span>$&</span>"));
-	$('#timebuffer').children().each(function( index ) {
+	var diff = $('<div>').html(_now.format(this._timeFormat+':mm').replace(/./g, "<span>$&</span>"));
+	diff.children().each(function( index ) {
 		var _text  = $( this ).text();
 		var _i = index+1;
-		var _text2 = $('#time span:nth-child('+_i+')').text();
+		var _text2 = $(timeLocation + ' span:nth-child('+_i+')').text();
 		if (_text != _text2) {
-			$('#time span:nth-child('+_i+')').addClass('fade');
+			$(timeLocation +' span:nth-child('+_i+')').addClass('fade');
 			$(this).addClass('fade');
 		}
 	});
 	$('.fade').fadeTo(400, 0.25, function() {
-		$('#time').html($('#timebuffer').html());
-		$('#time').children().fadeTo(400, 1).removeClass('fade');
-	});
+		$(timeLocation).html(diff.html());
+		$(timeLocation).children().fadeTo(400, 1).removeClass('fade');
+	}).bind(this);
 
 }
 
@@ -41,6 +40,7 @@ time.init = function () {
 	} else {
 		time._timeFormat = 'HH';
 	}
+	$(this.timeLocation).html('<span class="fade"></span>');
 
 	this.intervalId = setInterval(function () {
 		this.updateTime();
