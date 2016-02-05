60a7643dcdd4a0effc6e2972a754c1a931d62fd0
Jonathan Vogt <code@v-o-g-t.net>
2016-02-05 01:07:30 +0100

    Removed File Parsing Code
    @event15 @efiku 'volunteered' to write a better version.

--- controllers/hash.php
+++ controllers/hash.php
@@ -2,24 +2,10 @@
 function getHash() {
 	$hash = exec('git rev-parse HEAD', $output, $returncode);
 	if ($returncode == 0) {
-		return '0-'.$hash;
+		return '0-'.trim($hash);
 	}
-	// No git for php? Let's try parsing git structures
-	$file = dirname(str_replace('\\','/',__DIR__))."/.git/HEAD";
-	if (is_file($file)) {
-		$refOrCommit = file_get_contents($file);
-		if ($refOrCommit !== false && $refOrCommit !== null) {
-			if (strpos($refOrCommit, 'ref') !== false) {
-				list($ref, $head) = explode(' ', $refOrCommit);
-				$head = trim($head);
-				$file = dirname(str_replace('\\','/',__DIR__))."/.git/$head";
-				return '1-'.trim(file_get_contents($file));
-			} else {
-				return '2-'.trim($refOrCommit);
-			}
-		}
-		return 'static-version-sorry-no-auto-detect-of-changes';
-	}
+	// No git for php?
+	return 'static-version-sorry-no-auto-detect-of-changes';
 }
 
 if (str_replace('\\','/',__FILE__) == $_SERVER['SCRIPT_FILENAME']) {
