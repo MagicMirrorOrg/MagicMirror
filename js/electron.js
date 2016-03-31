9999b3aa0c7686d408bd76f0465e21627b372a1c
Paul-Vincent Roll <paviro@me.com>
2016-03-31 12:44:00 +0200

    Hide cursor and updated electron params

--- js/electron.js
+++ js/electron.js
@@ -23,7 +23,7 @@ let mainWindow;
 
 function createWindow () {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600,  fullscreen: true, autoHideMenuBar: true, nodeIntegration: false});
+	mainWindow = new BrowserWindow({width: 800, height: 600,  fullscreen: true, autoHideMenuBar: true, webPreferences: {nodeIntegration: false}});
 
 	// and load the index.html of the app.
 	//mainWindow.loadURL('file://' + __dirname + '../../index.html');
