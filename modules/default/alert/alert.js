5a612e792ebc3504db107c075a44c7b852f1ac4d
Paul-Vincent Roll <paviro@me.com>
2016-04-03 20:41:03 +0200

    Fixed mistake that happens when no type is given

--- modules/default/alert/alert.js
+++ modules/default/alert/alert.js
@@ -94,8 +94,8 @@ Module.register('alert',{
 		
 	},
 	notificationReceived: function(notification, payload, sender) {
-		if (typeof payload.type === 'undefined') { payload.type = "alert"; }
 		if (notification === 'SHOW_ALERT') {
+			if (typeof payload.type === 'undefined') { payload.type = "alert"; }
 			if (payload.type == "alert"){
 				this.show_alert(payload, sender)	
 			}
