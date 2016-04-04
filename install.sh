824eade8b2b4c3034a0eaf1c655ce29d086db350
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2016-04-04 12:28:07 -0400

    Update installation script for Pi 3 support.

--- install.sh
+++ install.sh
@@ -18,11 +18,12 @@ ARM=$(uname -m) # Determine which Pi is running.
 NODE_LATEST="v5.10.0" # Set the latest version here.
 6L_HASH="019a257faa5eebf6304686dfeffdbcb4c22f0547aa366f6e563aad39ab1b1ab1" # Set the armv6l hash here.
 7L_HASH="3f7524d3db60175c2323bb2a0a13ad1ca7d47d4ede6f42834b6b8425be70e0a2" # Set the armv7l hash here.
+8_HASH="df88803bda234b32240906b620315c8f6d6200332047a88cb0ec83009cf25dd5" # Set the arm64 hash here.
 DOWNLOAD_URL="https://nodejs.org/dist/latest/node-$NODE_LATEST-linux-$ARM.tar.gz" # Construct the download URL.
 wget $DOWNLOAD_URL # Download the file given.
 if [ $ARM = "armv6l" ]; then
 	if [ -f "node-$NODE_LATEST-linux-armv6l.tar.gz"]; then
-		COMMAND256 = "sha256sum node-$NODE_LATEST-linux-armv6l.tar.gz"
+		COMMAND256="sha256sum node-$NODE_LATEST-linux-armv6l.tar.gz"
 		if [ $($COMMAND256) = "019a257faa5eebf6304686dfeffdbcb4c22f0547aa366f6e563aad39ab1b1ab1" ]; then
 			echo "Node.js was downloaded and verified successfully."
 		else
@@ -32,8 +33,18 @@ if [ $ARM = "armv6l" ]; then
 	fi
 elif [ $ARM = "armv7l" ]; then
 	if [ -f "node-$NODE_LATEST-linux-armv7l.tar.gz" ]; then
-		COMMAND256 = "sha256sum node-$NODE_LATEST-linux-armv7l.tar.gz"
+		COMMAND256="sha256sum node-$NODE_LATEST-linux-armv7l.tar.gz"
 		if [ $($COMMAND256) = "3f7524d3db60175c2323bb2a0a13ad1ca7d47d4ede6f42834b6b8425be70e0a2" ]; then
+			echo "Node.js was downloaded and verified successfully."
+		else
+			echo "Node.js was downloaded, but verification failed. Make sure sha256sum works."
+			exit 1
+		fi
+	fi
+elif [ $ARM = "arm64" ]; then
+	if [ -f "node-$NODE_LATEST-linux-arm64.tar.gz" ]; then
+		COMMAND256="sha256sum node-$NODE_LATEST-linux-arm64.tar.gz"
+		if [ $($COMMAND256) = "df88803bda234b32240906b620315c8f6d6200332047a88cb0ec83009cf25dd5" ]; then
 			echo "Node.js was downloaded and verified successfully."
 		else
 			echo "Node.js was downloaded, but verification failed. Make sure sha256sum works."
