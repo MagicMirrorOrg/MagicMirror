f244537b89d559a7b27b522bf38491b37955eb1b
Drew Volz <volz.drew@gmail.com>
2016-04-07 15:54:23 -0500

    Update compliments.js
    Changes to comments

--- js/compliments/compliments.js
+++ js/compliments/compliments.js
@@ -22,9 +22,9 @@ compliments.updateCompliment = function () {
 
 	var hour = moment().hour();
 
-	// In the followign if statement we use .slice() on the
+	// In the following if statement we use .slice() on the
 	// compliments array to make a copy by value. 
-	// This way the original array of compliments stays in tact.
+	// This way the original array of compliments stays intact.
 
 	if (hour >= 3 && hour < 12) {
 		// Morning compliments
