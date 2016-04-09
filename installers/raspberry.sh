a88e4932e660c589440121a4cfb8aa0a6f2cae56
Michael Teeuw <michael@xonaymedia.nl>
2016-04-09 15:10:47 +0200

    Update installer.

--- installers/raspberry.sh
+++ installers/raspberry.sh
@@ -14,23 +14,26 @@
 # This is an installer script for MagicMirror2. It works well enough
 # that it can detect if you have Node installed, run a binary script
 # and then download and run MagicMirror2.
+
+echo "Installing helper tools ..."
 sudo apt-get install curl wget build-essential unzip || exit
 ARM=$(uname -m) # Determine which Pi is running.
 NODE_LATEST="v5.10.1" # Set the latest version here.
 DOWNLOAD_URL="https://nodejs.org/dist/latest/node-$NODE_LATEST-linux-$ARM.tar.gz" # Construct the download URL.
+
+echo "Installing Latest Node.js ..."
+mkdir ~/.MagicMirrorInstaller || exit
+cd  ~/.MagicMirrorInstaller || exit
 wget $DOWNLOAD_URL || exit # Download the file given.
 tar xvf node-$NODE_LATEST-linux-$ARM.tar.gz || exit
 cd node* || exit
 sudo cp -R * /usr/local || exit
-cd .. || exit
-rm -rf node* || exit
-# Run Node checks to make sure Node works properly.
-(curl -sL https://deb.nodesource.com/test | bash -) || exit
-npm config set loglevel info
-if [ ! -f package.json ]; then
-	wget https://github.com/nhubbard/MagicMirror/archive/v2-beta.zip
-	unzip v2-beta.zip
-	cd MagicMirror-2-beta
-fi
+cd ~ || exit
+rm -Rf ~/.MagicMirrorInstaller || exit
+
+echo "Cloning MagicMirror ..."
+git clone -b v2-beta https://github.com/MichMich/MagicMirror.git || exit
+cd MagicMirror  || exit
 npm install || exit
-echo "We're ready! Run `npm start` from the MagicMirror-2-beta directory (not over SSH) and enjoy MagicMirror2!"
+echo ""
+echo "We're ready! Run `DISPLAY=:0 npm start` from the MagicMirror directory.
