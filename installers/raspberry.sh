05b7237add7ae88f623e3af7bde241559fb0726e
Jordi De Poortere <jordi.de.poortere@me.com>
2016-05-13 00:02:36 +0200

    Fixing various typo's in the installer

--- installers/raspberry.sh
+++ installers/raspberry.sh
@@ -18,7 +18,7 @@ echo '                       \$$$$$$  |'
 echo '                        \______/'
 echo -e "\e[0m"
 
-# Define the tested version of node. 
+# Define the tested version of Node.js. 
 NODE_TESTED="v5.1.0"
 
 #Determine which Pi is running.
@@ -39,7 +39,7 @@ function command_exists () { type "$1" &> /dev/null ;}
 echo -e "\e[96mInstalling helper tools ...\e[90m"
 sudo apt-get install curl wget git build-essential unzip || exit
 
-# Check if we need to install or upgrade node.
+# Check if we need to install or upgrade Node.js.
 echo -e "\e[96mCheck current Node installation ...\e[0m"
 NODE_INSTALL=false
 if command_exists node; then
@@ -60,29 +60,33 @@ if command_exists node; then
 		fi
 
     else
-    	echo -e "\e[92mNo Node upgrade nessecery.\e[0m"
+    	echo -e "\e[92mNo Node.js upgrade nessecery.\e[0m"
 	fi
 
 else
-	echo -e "\e[93mNode is not installed.\e[0m";
+	echo -e "\e[93mNode.js is not installed.\e[0m";
 	NODE_INSTALL=true
 fi
 
 # Install or upgare node if nessecery.
 if $NODE_INSTALL; then
 	
+	echo -e "\e[96mInstalling Node.js ...\e[90m"
+
 	#Fetch the latest version of Node.js from the selected branch
 	#The NODE_STABLE_BRANCH variable will need to be manually adjusted when a new branch is released. (e.g. 7.x)
 	#Only tested (stable) versions are recommended as newer versions could break MagicMirror.
+	
 	NODE_STABLE_BRANCH="6.x"
 	curl -sL https://deb.nodesource.com/setup_$NODE_STABLE_BRANCH | sudo -E bash -
 	sudo apt-get install -y nodejs
+	echo -e "\e[92mNode.js installation Done!\e[0m"
 fi
 
 #Install magic mirror
 cd ~
 if [ -d "$HOME/MagicMirror" ] ; then
-	echo -e "\e[93mIt seems like MagicMirror is allready installed."
+	echo -e "\e[93mIt seems like MagicMirror is already installed."
 	echo -e "To prevent overwriting, the installer will be aborted."
 	echo -e "Please rename the \e[1m~/MagicMirror\e[0m\e[93m folder and try again.\e[0m"
 	echo ""
@@ -109,6 +113,6 @@ else
 fi
 
 echo " "
-echo -e "\e[92mWe're ready! Run \e[1m\e[97mDISPLAY=:0 npm start\e[0m\e[92m from the ~/MagicMirror directory to start your MagicMirror."
+echo -e "\e[92mWe're ready! Run \e[1m\e[97mDISPLAY=:0 npm start\e[0m\e[92m from the ~/MagicMirror directory to start your MagicMirror.\e[0m"
 echo " "
 echo " "
