3d174acd0f751cb7f5082ad116fe8521f16ed299
Ashley M. Kirchner <kirash4@gmail.com>
2016-06-02 00:30:48 -0600

    Continied Calendar2 development

--- modules/calendar2/calendar2.js
+++ modules/calendar2/calendar2.js
@@ -10,13 +10,27 @@
 	// Module defaults
 	defaults: {
 		updateInterval: 60 * 1000,	// every minute
-		fadeSpeed: 3 * 1000,		// fade out and in for 3 seconds
-		view: "monthly"
+		fadeSpeed: 2 * 1000,		// fade out and in for 3 seconds
+		displayHeader: true,
+		view: "monthly",
+		cssStyle: "default"
 	},
 
 	// Required styles
 	getStyles: function() {
-		return ["calendar2.css"];
+		switch(this.config.cssStyle) {
+			case "blue":
+				return ["styleDefault.css", "styleBlue.css"];
+				break;
+			case "block":
+				return ["styleDefault.css", "styleBlock.css"];
+				break;
+			case "custom":
+				return ["styleDefault.css", "styleCustom.css"];
+				break;
+			default:
+				return ["styleDefault.css"];
+		}
 	},
 	
 	// Required scripts
@@ -35,7 +49,7 @@
 		var self = this;
 		setInterval(function() {
 			self.updateDom(self.config.fadeSpeed);
-			Log.log("Calendar2 updating at " + moment().format("hh:mm"));
+			//Log.log("Calendar2 updating at " + moment().format("hh:mm"));
 		}, this.config.updateInterval);
 	},
 	
@@ -48,6 +62,8 @@
 		// Find first day of the month
 		var firstDay = moment().date(1);
 		var startingDay = firstDay.day();
+		Log.log("firstDay: " + firstDay);
+		Log.log("startingDay: " + startingDay);
 
 		// Find number of days in month
 		var monthLength = moment().daysInMonth();
@@ -56,13 +72,15 @@
 		wrapper.className = 'xsmall';
 	
 		// Header
-		var monthName = moment().format("MMM");
+		var monthName = moment().format("MMMM");
 		var html = '<table id="calendar-table">';
 
 		html += '<thead>';
-		html += '<tr><th scope="col" colspan="7" id="calendar-th">';
-		html += monthName + "&nbsp;" + year;
-		html += '</th></tr>';
+		if (this.config.displayHeader) {
+			html += '<tr><th scope="col" colspan="7" id="calendar-th">';
+			html += '<span id="monthName">' + monthName + '</span>&nbsp;<span id="yearDigits">' + year + '</span>';
+			html += '</th></tr>';
+		}
 		html += '</thead>';
 	
 		html += '<tfoot>';
@@ -80,19 +98,36 @@
 
 		// Fill in the days
 		var day = 1;
+		var nextMonth = 1;
 		// Loop for amount of weeks (as rows)
 		for (var i = 0; i < 9; i++) {
 			// Loop for each weekday (as individual cells)
 			for (var j = 0; j <= 6; j++) {
-				if (day == moment().date()) {
-					html += '<td class="calendar-day today">';
-				} else {
-					html += '<td class="calendar-day">';
-				}
-				if (day <= monthLength && (i > 0 || j >= startingDay)) {
+				html += '<td class="calendar-day">';
+				html += '<div class="square-box">';
+				html += '<div class="square-content"><div>';
+				
+				if (j < startingDay && i == 0) {
+					// First row, fill in empty slots
+					html+= '<span class="monthPrevNext">';
+					html+= moment().subtract(1, 'months').endOf('month').subtract((startingDay - 1) - j, 'days').date();
+				} else if (day <= monthLength && (i > 0 || j >= startingDay)) {
+					if (day == moment().date()) {
+						html += '<span id="today">';
+					} else {
+						html += '<span>';
+					}
 					html += day;
+					html += '</span>';
 					day++;
+				} else if (day > monthLength && i > 0) {
+					// Last row, fill in empty space
+					html += '<span class="monthPrevNext">';
+					html += moment([year, month, monthLength]).add(nextMonth, 'days').date();
+					nextMonth++;
 				}
+				
+				html += '</div></div></div>';
 				html += '</td>';
 			}
 			// Don't need any more rows if we've run out of days
