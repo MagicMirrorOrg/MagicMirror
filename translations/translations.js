611627ca97a9828fdf39e5889eb3ac54359330de
Michael Teeuw <michael@xonaymedia.nl>
2016-06-04 21:36:03 +0200

    Fix NE translations for all languages.

--- translations/translations.js
+++ translations/translations.js
@@ -6,15 +6,15 @@
  */
 
 var translations = {
-	"en" : "translations/en.json",
-	"nl" : "translations/nl.json",
-	"de" : "translations/de.json",
-	"fr" : "translations/fr.json",
-	"fy" : "translations/fy.json",
-	"es" : "translations/es.json",
-	"nb" : "translations/nb.json",
-	"nn" : "translations/nn.json",
-	"pt" : "translations/pt.json",
-	"sv" : "translations/sv.json",
-	"it" : "translations/it.json",
+	"en" : "translations/en.json", // English
+	"nl" : "translations/nl.json", // Dutch
+	"de" : "translations/de.json", // German
+	"fr" : "translations/fr.json", // French
+	"fy" : "translations/fy.json", // Frysk
+	"es" : "translations/es.json", // Spanish
+	"nb" : "translations/nb.json", // Norsk bokmål
+	"nn" : "translations/nn.json", // Norsk nynorsk
+	"pt" : "translations/pt.json", // Português
+	"sv" : "translations/sv.json", // Svenska
+	"it" : "translations/it.json", // Italian
 };
