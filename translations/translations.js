28ca8a9f9ffe397d778df3ebb9f6d999b0cba170
Diego Vieira <diego@protos.inf.br>
2016-07-26 07:31:31 -0300

    added Greek translation back

--- translations/translations.js
+++ translations/translations.js
@@ -22,5 +22,5 @@ var translations = {
 	"zh_tw" : "translations/zh_tw.json", // Traditional Chinese
 	"ja" : "translations/ja.json", // Japanese
 	"pl" : "translations/pl.json", // Polish
-
+	"gr" : "translations/gr.json",  // Greek
 };
