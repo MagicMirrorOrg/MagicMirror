c427050a92be5c2fea16281c91798260b62a4049
yo-less <jens.olbertz@gmail.com>
2016-08-27 01:16:31 +0200

    Allow for the removal of feed tags
    Added options: both, description, title

--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -25,8 +25,8 @@ Module.register("newsfeed",{
 		updateInterval: 10 * 1000,
 		animationSpeed: 2.5 * 1000,
 		maxNewsItems: 0, // 0 for unlimited
-		removeStartTags: false,
-		removeEndTags: false,
+		removeStartTags: '',
+		removeEndTags: '',
 		startTags: [],
 		endTags: []
 		
@@ -103,13 +103,17 @@ Module.register("newsfeed",{
 
 			//Remove selected tags from the beginning of rss feed items (title or description)
 
-			if (this.config.removeStartTags) {
+			if (this.config.removeStartTags == 'title' || 'both') {
 				
 				for (f=0; f<this.config.startTags.length;f++) {
 					if (this.newsItems[this.activeItem].title.slice(0,this.config.startTags[f].length) == this.config.startTags[f]) {
 						this.newsItems[this.activeItem].title = this.newsItems[this.activeItem].title.slice(this.config.startTags[f].length,this.newsItems[this.activeItem].title.length);
 						}
 				}
+				
+			}
+				
+			if (this.config.removeStartTags == 'description' || 'both') {
 				
 				if (this.config.showDescription) {
 					for (f=0; f<this.config.startTags.length;f++) {
