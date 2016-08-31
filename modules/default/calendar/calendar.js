1b69e62e2e25497959614bb3e0d83b5af05659ce
Jens <jens.olbertz@gmail.com>
2016-08-31 14:39:37 +0200

    Added "DAYAFTERTOMORROW" tag if config language is German

--- modules/default/calendar/calendar.js
+++ modules/default/calendar/calendar.js
@@ -150,6 +150,12 @@ Module.register("calendar",{
 					timeWrapper.innerHTML = this.translate("TODAY");
 				} else if (event.startDate - now < one_day && event.startDate - now > 0) {
 					timeWrapper.innerHTML = this.translate("TOMORROW");
+				} else if (event.startDate - now < 2*one_day && event.startDate - now > 0) {
+					if (config.language == 'de') {
+						timeWrapper.innerHTML = this.translate("DAYAFTERTOMORROW");
+					} else {
+						timeWrapper.innerHTML = moment(event.startDate, "x").fromNow();
+					}
 				} else {
 					/* Check to see if the user displays absolute or relative dates with their events
 					 * Also check to see if an event is happening within an 'urgency' time frameElement
