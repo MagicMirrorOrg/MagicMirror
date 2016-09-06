af279df5c9845b3063d906b441ddcddef9a4493c
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2016-09-06 19:23:52 -0400

    Closes #374.
    ARMv6 is not supported by recent versions of Chrome, and by extent, this means recent versions of Electron as well.

--- installers/raspberry.sh
+++ installers/raspberry.sh
@@ -28,6 +28,7 @@ ARM=$(uname -m)
 if [ "$ARM" != "armv7l" ]; then
 	echo -e "\e[91mSorry, your Raspberry Pi is not supported."
 	echo -e "\e[91mPlease run MagicMirror on a Raspberry Pi 2 or 3."
+	echo -e "\e[91mIf this is a Pi Zero, you are in the same boat as the original Raspberry Pi. You must run in server only mode."
 	exit;
 fi
 
