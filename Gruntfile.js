88572e90d48333ddf0c64f4839bf985766d7d68f
Nicholas Hubbard <hubbardn95@gmail.com>
2016-09-10 16:41:57 -0400

    Add all the linters

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,53 @@ module.exports = function(grunt) {
 				},
 				src: ["css/main.css", "modules/default/calendar/calendar.css", "modules/default/clock/clock_styles.css", "modules/default/currentweather/currentweather.css", "modules/default/weatherforcast/weatherforcast.css"]
 			}
+		},
+		jsonlint: {
+			main: {
+				src: ["package.json", ".eslintrc.json", ".stylelint"],
+				options: {
+					reporter: "jshint"
+				}
+			}
+		},
+		markdownlint: {
+			all: {
+				options: {
+					config: {
+						"default": true,
+						"line-length": false,
+						"blanks-around-headers": false,
+						"no-duplicate-header": false,
+						"no-inline-html": false,
+						"MD010": false,
+						"MD001": false,
+						"MD031": false,
+						"MD040": false,
+						"MD002": false,
+						"MD029": false,
+						"MD041": false,
+						"MD032": false,
+						"MD036": false,
+						"MD037": false,
+						"MD009": false,
+						"MD018": false,
+						"MD012": false,
+						"MD026": false,
+						"MD036": false,
+						"MD038": false
+					}
+				},
+				src: ["README.md", "CHANGELOG.md", "LICENSE.md", "modules/README.md", "modules/default/**/*.md", "!modules/default/calendar/vendor/ical.js/readme.md"]
+			}
+		},
+		yamllint: {
+			all: [".travis.yml"]
 		}
 	});
 	grunt.loadNpmTasks("grunt-eslint");
 	grunt.loadNpmTasks("grunt-stylelint");
-	grunt.registerTask("default", ["eslint", "stylelint"]);
+	grunt.loadNpmTasks("grunt-jsonlint");
+	grunt.loadNpmTasks("grunt-yamllint");
+	grunt.loadNpmTasks("grunt-markdownlint");
+	grunt.registerTask("default", ["eslint", "stylelint", "jsonlint", "markdownlint", "yamllint"]);
 };
