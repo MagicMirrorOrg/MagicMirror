95f1382fbfd4b4db0de8c9dcc1bda6dbbb6873fd
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-10-24 23:53:34 -0300

    add support to enable set timezone for show in clock module.

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -9,6 +9,7 @@
 
 var vendor = {
 	'moment.js' : 'moment/moment-with-locales.js',
+	'moment-timezone.js' : 'moment/moment-timezone.js',
 	'weather-icons.css': 'weather-icons/css/weather-icons.css',
 	'weather-icons-wind.css': 'weather-icons/css/weather-icons-wind.css',
 	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css'
