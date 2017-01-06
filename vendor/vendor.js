e5630a7d55c39881e9275b44798a082027d9dcab
Chris van Marle <qistoph@gmail.com>
2017-01-06 18:22:34 +0100

    Add chart.js to vendor scripts

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -12,5 +12,6 @@ var vendor = {
 	'moment-timezone.js' : 'moment/moment-timezone.js',
 	'weather-icons.css': 'weather-icons/css/weather-icons.css',
 	'weather-icons-wind.css': 'weather-icons/css/weather-icons-wind.css',
-	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css'
+	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css',
+	'Chart.js' : 'chartjs/Chart.js'
 };
