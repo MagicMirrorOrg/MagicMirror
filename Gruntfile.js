690567659cc7bbc8d11ae1618c1c0ac67cc1132d
Sergey Morozov <sergey@slack-corp.com>
2017-01-23 09:46:50 -0800

    Minor clean up for test suite
    @roramirez thanks for starting adding tests. I figured that we might as well grunt them and follow same rules for linting as we do for rest of JS code in the repo.
    
    I've made following minor modifications:
    - added tests to the grunt target
    - fixed indentation in package.json
    - made tests a bit more descriptive
    - fixed eslint errors surfaced by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function(grunt) {
 				configFile: ".eslintrc.json"
 			},
 			target: ["js/*.js", "modules/default/*.js", "modules/default/*/*.js",
-				"serveronly/*.js", "*.js", "!modules/default/alert/notificationFx.js",
-				"!modules/default/alert/modernizr.custom.js", "!modules/default/alert/classie.js"
+				"serveronly/*.js", "*.js", "tests/*/*.js", "!modules/default/alert/notificationFx.js",
+				"!modules/default/alert/modernizr.custom.js", "!modules/default/alert/classie.js",
 			]
 		},
 		stylelint: {
