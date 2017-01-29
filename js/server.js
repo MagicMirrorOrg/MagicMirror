0d8d8f042692b249f046a1d488ca58782656373d
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-01-28 23:23:32 -0300

    Add tests configs directory as  express route.

--- js/server.js
+++ js/server.js
@@ -37,6 +37,7 @@ var Server = function(config, callback) {
 	app.use("/modules", express.static(path.resolve(global.root_path + "/modules")));
 	app.use("/vendor", express.static(path.resolve(global.root_path + "/vendor")));
 	app.use("/translations", express.static(path.resolve(global.root_path + "/translations")));
+	app.use("/tests/confs", express.static(path.resolve(global.root_path + "/tests/confs")));
 
 	app.get("/version", function(req,res) {
 		res.send(global.version);
