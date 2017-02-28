6e4aad1d41be8a23070bd2ef50ac553cc7a3b65a
StephenJCotton <cot.s.ton@gmail.com>
2017-02-28 07:47:16 -0500

    Additional Vendor Libraries

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -10,6 +10,9 @@
 var vendor = {
 	'moment.js' : 'moment/moment-with-locales.js',
 	'moment-timezone.js' : 'moment/moment-timezone.js',
+	'jquery.js' : 'jquery/jquery.',
+	'underscore.js' : 'underscorejs/underscore-min.js',
+	'backbone.js' : 'underscorejs/backbone-min.js',
 	'weather-icons.css': 'weather-icons/css/weather-icons.css',
 	'weather-icons-wind.css': 'weather-icons/css/weather-icons-wind.css',
 	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css'
