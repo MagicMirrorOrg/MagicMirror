11fe6cfbb0674572a7724e4283a3437b00fa8e42
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-03-07 21:22:27 -0300

    Add test  ipWhistelist = []
    This test ipWhistelist on [] to access all IPs
    Creates a request to localhost and port added in configuration and
    check if gets 200 HTTP code.

--- tests/e2e/ipWhistlist_spec.js
+++ tests/e2e/ipWhistlist_spec.js
@@ -4,14 +4,10 @@ const request = require("request");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("Set ipWhitelist without access", function () {
+
+describe("ipWhitelist directive configuration", function () {
 
 	this.timeout(20000);
-
-	before(function() {
-		// Set config sample for use in test
-		process.env.MM_CONFIG_FILE = "tests/configs/noIpWhiteList.js";
-	});
 
 	beforeEach(function (done) {
 		app.start().then(function() { done(); } );
@@ -21,10 +17,30 @@ describe("Set ipWhitelist without access", function () {
 		app.stop().then(function() { done(); });
 	});
 
-	it("should return 403", function (done) {
-		request.get("http://localhost:8080", function (err, res, body) {
-			expect(res.statusCode).to.equal(403);
-			done();
+	describe("Set ipWhitelist without access", function () {
+		before(function() {
+			// Set config sample for use in test
+			process.env.MM_CONFIG_FILE = "tests/configs/noIpWhiteList.js";
+		});
+		it("should return 403", function (done) {
+			request.get("http://localhost:8080", function (err, res, body) {
+				expect(res.statusCode).to.equal(403);
+				done();
+			});
 		});
 	});
+
+	describe("Set ipWhitelist []", function () {
+		before(function() {
+			// Set config sample for use in test
+			process.env.MM_CONFIG_FILE = "tests/configs/empty_ipWhiteList.js";
+		});
+		it("should return 200", function (done) {
+			request.get("http://localhost:8080", function (err, res, body) {
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+		});
+	});
+
 });
