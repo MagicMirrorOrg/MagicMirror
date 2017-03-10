cfc8117c3c067150ad133c24f7b84dc01c8763db
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-03-10 00:52:43 -0300

    Remove tests case  from without_modules with much hardcode
    This test retain
     * Check title.
     * Check footer with Michael website.

--- tests/e2e/without_modules.js
+++ tests/e2e/without_modules.js
@@ -44,21 +44,6 @@ describe("Check configuration without modules", function () {
 			.getText("#module_1_helloworld .module-content").should.eventually.equal("Magic Mirror2")
 	});
 
-	it("Show the message create file config", function () {
-		return app.client.waitUntilWindowLoaded()
-			.getText("#module_2_helloworld .module-content").should.eventually.equal("Please create a config file.")
-	});
-
-	it("Show the message See more information in README", function () {
-		return app.client.waitUntilWindowLoaded()
-			.getText("#module_3_helloworld .module-content").should.eventually.equal("See README for more information.")
-	});
-
-	it("Show the message recomended use a linter for Javascript for check configuration", function () {
-		return app.client.waitUntilWindowLoaded()
-			.getText("#module_4_helloworld .module-content").should.eventually.equal("If you get this message while your config file is already\ncreated, your config file probably contains an error.\nUse a JavaScript linter to validate your file.")
-	});
-
 	it("Show the text Michael's website", function () {
 		return app.client.waitUntilWindowLoaded()
 			.getText("#module_5_helloworld .module-content").should.eventually.equal("www.michaelteeuw.nl");
