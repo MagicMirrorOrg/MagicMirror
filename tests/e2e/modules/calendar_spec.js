1c235aa7611dd4f8912bce2b01e2abdba16eef26
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-03-10 03:25:16 -0300

    Add test default calendar

---
+++ tests/e2e/modules/calendar_spec.js
@@ -0,0 +1,29 @@
+const globalSetup = require("../global-setup");
+const app = globalSetup.app;
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Calendar module", function () {
+
+	this.timeout(20000);
+
+	beforeEach(function (done) {
+		app.start().then(function() { done(); } );
+	});
+
+	afterEach(function (done) {
+		app.stop().then(function() { done(); });
+	});
+
+	describe("Default configuration", function() {
+		before(function() {
+			// Set config sample for use in test
+			process.env.MM_CONFIG_FILE = "tests/configs/modules/calendar/default.js";
+		});
+
+		it("Should return TestEvents", function () {
+			return app.client.waitUntilTextExists(".calendar", "TestEvent", 10000);
+		});
+	});
+
+});
