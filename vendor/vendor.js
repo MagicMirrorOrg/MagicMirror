328f15c2eabcf74d3bf260a69d3acad8d3f58baa
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-03-12 21:55:08 -0300

    Add font-awesome using npm on vendors:
    This can updated libs and clean repository code.

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -12,5 +12,5 @@ var vendor = {
 	'moment-timezone.js' : 'moment/moment-timezone.js',
 	'weather-icons.css': 'weather-icons/css/weather-icons.css',
 	'weather-icons-wind.css': 'weather-icons/css/weather-icons-wind.css',
-	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css'
+	'font-awesome.css': 'node_modules/font-awesome/css/font-awesome.min.css'
 };
