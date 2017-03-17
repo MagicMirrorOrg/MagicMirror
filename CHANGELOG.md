a18e7eb0894ad1aa840c2e5e4d4827158a9f706d
Andrew McOlash <amcolash@gmail.com>
2017-03-17 07:08:00 -0500

    Fix conflict with CHANGELOG, merge with upstream/develop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -49,7 +49,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 - Korean Translation.
 - Added console warning on startup when deprecated config options are used
 - Added `DAYAFTERTOMORROW`, `UPDATE_NOTIFICATION`, `UPDATE_NOTIFICATION_MODULE`, `UPDATE_INFO` to Norwegian translations (`nn` and `nb`).
-- Add option to display temperature unit label
+- Add option to display temperature unit label for current weather module
 
 ### Fixed
 - Update .gitignore to not ignore default modules folder.
--- modules/default/calendar/debug.js
+++ modules/default/calendar/debug.js
@@ -8,14 +8,22 @@
 
 var CalendarFetcher = require("./calendarfetcher.js");
 
-var url = "https://calendar.google.com/calendar/ical/pkm1t2uedjbp0uvq1o7oj1jouo%40group.calendar.google.com/private-08ba559f89eec70dd74bbd887d0a3598/basic.ics";
+var url = "https://calendar.google.com/calendar/ical/pkm1t2uedjbp0uvq1o7oj1jouo%40group.calendar.google.com/private-08ba559f89eec70dd74bbd887d0a3598/basic.ics";  // Standard test URL
+// var url = "https://www.googleapis.com/calendar/v3/calendars/primary/events/"; // URL for Bearer auth (must be configured  in Google OAuth2 first)
 var fetchInterval = 60 * 60 * 1000;
 var maximumEntries = 10;
 var maximumNumberOfDays = 365;
+var user = "magicmirror";
+var pass = "MyStrongPass";
+
+var auth = {
+	user: user,
+	pass: pass
+};
 
 console.log("Create fetcher ...");
 
-fetcher = new CalendarFetcher(url, fetchInterval, maximumEntries, maximumNumberOfDays);
+fetcher = new CalendarFetcher(url, fetchInterval, maximumEntries, maximumNumberOfDays, auth);
 
 fetcher.onReceive(function(fetcher) {
 	console.log(fetcher.events());
