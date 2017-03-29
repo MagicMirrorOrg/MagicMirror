af9fdfa224d28ecbf958221b720cb3fe5979f8e2
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-03-28 22:15:48 -0300

    Add roundValue unit test function currentweather module:     - tests for this.config.roundTemp is false

--- tests/unit/functions/currentweather_spec.js
+++ tests/unit/functions/currentweather_spec.js
@@ -7,21 +7,23 @@ var vm = require("vm");
 
 describe("Functions module currentweather", function() {
 
-	// Fake for use by calendar.js
-	Module = {}
-	Module.definitions = {};
-	Module.register = function (name, moduleDefinition) {
-		Module.definitions[name] = moduleDefinition;
-	};
-	config = {};
+	before(function(){
+		Module = {};
+		config = {};
+		Module.definitions = {};
+		Module.register = function (name, moduleDefinition) {
+			Module.definitions[name] = moduleDefinition;
+		};
+		require("../../../modules/default/currentweather/currentweather.js");
+		Module.definitions.currentweather.config = {};
+	});
 
 	describe("roundValue", function() {
-		describe("this.config.roundTemp is true", function() {
-			// load currentweather
-			require("../../../modules/default/currentweather/currentweather.js");
 
-			Module.definitions.currentweather.config = {};
-			Module.definitions.currentweather.config.roundTemp = true;
+		describe("this.config.roundTemp is true", function() {
+			before(function(){
+				Module.definitions.currentweather.config.roundTemp = true;
+			});
 
 			var values = [
 				// index 0 value
@@ -33,6 +35,33 @@ describe("Functions module currentweather", function() {
 				[2.0    ,  "2"],
 				["2.12" ,  "2"],
 				[10.1   , "10"]
+			]
+
+			values.forEach(value => {
+				it(`for ${value[0]} should be return ${value[1]}`, function() {
+					expect(Module.definitions.currentweather.roundValue(value[0])).to.equal(value[1]);
+				});
+			});
+		});
+
+
+		describe("this.config.roundTemp is false", function() {
+
+			before(function(){
+				Module.definitions.currentweather.config.roundTemp = false;
+			});
+
+			var values = [
+				// index 0 value
+				// index 1 expect
+				[1      ,  "1.0"],
+				[1.0    ,  "1.0"],
+				[1.02   ,  "1.0"],
+				[10.12  , "10.1"],
+				[2.0    ,  "2.0"],
+				["2.12" ,  "2.1"],
+				[10.1   , "10.1"],
+				[10.10  , "10.1"]
 			]
 
 			values.forEach(value => {
