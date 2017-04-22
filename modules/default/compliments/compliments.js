778107aae9dd022116c2ee575ebda6efa2b778f2
fewieden <fewieden@htwg-konstanz.de>
2017-04-22 12:09:52 +0200

    add edge case anytime only

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -98,11 +98,11 @@ Module.register("compliments", {
 		var hour = moment().hour();
 		var compliments = null;
 
-		if (hour >= 3 && hour < 12) {
+		if (hour >= 3 && hour < 12 && this.config.compliments.hasOwnProperty("morning")) {
 			compliments = this.config.compliments.morning.slice(0);
-		} else if (hour >= 12 && hour < 17) {
+		} else if (hour >= 12 && hour < 17 && this.config.compliments.hasOwnProperty("afternoon")) {
 			compliments = this.config.compliments.afternoon.slice(0);
-		} else {
+		} else if(this.config.compliments.hasOwnProperty("evening")) {
 			compliments = this.config.compliments.evening.slice(0);
 		}
 
