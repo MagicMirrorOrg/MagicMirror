0de65d9c0f00258673f19a760f29e12d2a27889f
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-05-11 02:30:25 -0300

    Fix spelling comment js/loader.js

--- js/loader.js
+++ js/loader.js
@@ -32,8 +32,8 @@ var Loader = (function() {
 				});
 			} else {
 				// All modules loaded. Load custom.css
-				// This is done after all the moduels so we can
-				// overwrite all the defined styls.
+				// This is done after all the modules so we can
+				// overwrite all the defined styles.
 
 				loadFile("css/custom.css", function() {
 					// custom.css loaded. Start all modules.
