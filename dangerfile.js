27b3875bfb93c698646904c32ab55ced51339b6b
Michael Teeuw <michael@xonaymedia.nl>
2018-01-25 20:38:42 +0100

    Changed missing Changlog text.

--- dangerfile.js
+++ dangerfile.js
@@ -5,5 +5,5 @@ const changelogEdited = danger.git.modified_files.includes("CHANGELOG.md")
 
 // Fail the build and post a comment reminding submitters to do so if it wasn't changed
 if (!changelogEdited) {
-	fail("Please include a CHANGELOG entry. You can find it at [CHANGELOG.md](CHANGELOG.md).")
+	fail("Please include an updated `CHANGELOG.md` file.<br>This way we can keep track of all the contributions.<br><br>Thanks!")
 }
