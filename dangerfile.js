fe4ffeb7f172e381717af37775d14c1834fc33bd
Michael Teeuw <michael@xonaymedia.nl>
2018-01-25 20:45:25 +0100

    Text cleanup.

--- dangerfile.js
+++ dangerfile.js
@@ -5,5 +5,5 @@ const changelogEdited = danger.git.modified_files.includes("CHANGELOG.md")
 
 // Fail the build and post a comment reminding submitters to do so if it wasn't changed
 if (!changelogEdited) {
-	fail("Please include an updated `CHANGELOG.md` file.<br>This way we can keep track of all the contributions.<br><br>Thanks!")
+	fail("Please include an updated `CHANGELOG.md` file.<br>This way we can keep track of all the contributions.")
 }
