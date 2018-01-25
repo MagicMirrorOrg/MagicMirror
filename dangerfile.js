e2dbe8a0a23e36f07e0f71157099ca199359a483
Michael Teeuw <michael@xonaymedia.nl>
2018-01-25 20:07:51 +0100

    Minor fixes.

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,9 @@
-import { danger, fail } from 'danger'
+import { danger, fail } from "danger"
 
 // Check if the CHANGELOG.md file has been edited
-const changelogEdited = danger.git.modified_files.includes('CHANGELOG.md')
+const changelogEdited = danger.git.modified_files.includes("CHANGELOG.md")
 
 // Fail the build and post a comment reminding submitters to do so if it wasn't changed
 if (!changelogEdited) {
-	fail('Please include a CHANGELOG entry. You can find it at [CHANGELOG.md](CHANGELOG.md).')
+	fail("Please include a CHANGELOG entry. You can find it at [CHANGELOG.md](CHANGELOG.md).")
 }
