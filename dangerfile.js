29fc7910b711ccc7ea24fbbdef974c9509640c52
Michael Teeuw <michael@xonaymedia.nl>
2018-01-25 19:51:23 +0100

    Remove Jest. Update dangerfile.js.

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,9 @@
-import { message, danger } from "danger"
+import { danger, fail } from 'danger'
 
-// Add a CHANGELOG entry for app changes
-const hasChangelog = includes(danger.git.modified_files, "CHANGELOG.md")
-const isTrivial = contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
+// Check if the CHANGELOG.md file has been edited
+const changelogEdited = danger.git.modified_files.includes('CHANGELOG.md')
 
-if (!hasChangelog && !isTrivial) {
-    warn("Please add a `CHANGELOG.md` entry for your changes.")
+// Fail the build and post a comment reminding submitters to do so if it wasn't changed
+if (!changelogEdited) {
+	fail('Please include a CHANGELOG entry. You can find it at [CHANGELOG.md](CHANGELOG.md).')
 }
