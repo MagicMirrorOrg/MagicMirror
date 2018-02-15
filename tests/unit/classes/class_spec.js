3e2a1e354842d40ebd3bd8f19381f8fc7a8981ee
fewieden <felix@wiedenba.ch>
2018-02-16 00:08:11 +0100

    linting

--- tests/unit/classes/class_spec.js
+++ tests/unit/classes/class_spec.js
@@ -5,41 +5,41 @@ const {JSDOM} = require("jsdom");
 
 describe("File js/class", function() {
 	describe("Test function cloneObject", function() {
-        let clone;
+		let clone;
 
-        before(function(done) {
-            const dom = new JSDOM(`<script src="${path.join(__dirname, "..", "..", "..", "js", "class.js")}">`, { runScripts: "dangerously",
-                resources: "usable" });
-            dom.window.onload = function() {
-                const {cloneObject} = dom.window;
-                clone = cloneObject;
-                done();
-            };
-        });
+		before(function(done) {
+			const dom = new JSDOM(`<script src="${path.join(__dirname, "..", "..", "..", "js", "class.js")}">`, { runScripts: "dangerously",
+				resources: "usable" });
+			dom.window.onload = function() {
+				const {cloneObject} = dom.window;
+				clone = cloneObject;
+				done();
+			};
+		});
 
 		it("should be return equals object", function() {
-            const expected = {name: "Rodrigo", web: "https://rodrigoramirez.com", project: "MagicMirror"};
-            let obj = {};
+			const expected = {name: "Rodrigo", web: "https://rodrigoramirez.com", project: "MagicMirror"};
+			let obj = {};
 			obj = clone(expected);
 			expect(expected).to.deep.equal(obj);
 		});
 
 		it("should be return equals int", function() {
-            const expected = 1;
+			const expected = 1;
 			let obj = {};
 			obj = clone(expected);
 			expect(expected).to.equal(obj);
 		});
 
 		it("should be return equals string", function() {
-            const expected = "Perfect stranger";
+			const expected = "Perfect stranger";
 			let obj = {};
 			obj = clone(expected);
 			expect(expected).to.equal(obj);
 		});
 
 		it("should be return equals undefined", function() {
-            const expected = undefined;
+			const expected = undefined;
 			let obj = {};
 			obj = clone(expected);
 			expect(undefined).to.equal(obj);
