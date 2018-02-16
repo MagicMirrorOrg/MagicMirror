a756fed70b4a3ebce1cd9cf5a4749dae46403c2e
fewieden <felix@wiedenba.ch>
2018-02-16 08:43:27 +0100

    fixed and reenabled lockstring test

--- tests/unit/classes/class_spec.js
+++ tests/unit/classes/class_spec.js
@@ -6,9 +6,11 @@ const {JSDOM} = require("jsdom");
 describe("File js/class", function() {
 	describe("Test function cloneObject", function() {
 		let clone;
+		let dom;
 
 		before(function(done) {
-			const dom = new JSDOM(`<script src="${path.join(__dirname, "..", "..", "..", "js", "class.js")}">`, { runScripts: "dangerously",
+			dom = new JSDOM(`<script>var Log = {log: function() {}};</script>\
+					<script src="${path.join(__dirname, "..", "..", "..", "js", "class.js")}">`, { runScripts: "dangerously",
 				resources: "usable" });
 			dom.window.onload = function() {
 				const {cloneObject} = dom.window;
@@ -45,17 +47,28 @@ describe("File js/class", function() {
 			expect(undefined).to.equal(obj);
 		});
 
-		// CoverageME
-		/*
-		context("Test lockstring code", function() {
-			it("should be return equals object", function() {
-				const expected = {name: "Module", lockStrings: "stringLock"};
+		describe("Test lockstring code", function() {
+			let log;
+
+			before(function() {
+                log = dom.window.Log.log;
+                dom.window.Log.log = function cmp(str) {
+                    expect(str).to.equal("lockStrings");
+                };
+			});
+
+            after(function() {
+                dom.window.Log.log = log;
+            });
+
+			it("should be return equals object and log lockStrings", function() {
+                const expected = {name: "Module", lockStrings: "stringLock"};
 				let obj = {};
 				obj = clone(expected);
 				expect(expected).to.deep.equal(obj);
 			});
+
 		});
-		*/
 
 	});
 });
