7159ab3ef2e0ba2714477cae7e9d232c2a752de4
carsten <carsten@refinedlabs.com>
2018-06-08 10:41:55 +0200

    - code styling

--- modules/default/currentweather/currentweather.js
+++ modules/default/currentweather/currentweather.js
@@ -252,7 +252,7 @@ Module.register("currentweather",{
 			var feelsLike = document.createElement("span");
 			feelsLike.className = "dimmed";
 			feelsLike.innerHTML = this.translate("FEELS") + " " + this.feelsLike + "&deg;" + degreeLabel;
-            small.appendChild(feelsLike);
+			small.appendChild(feelsLike);
 
 			wrapper.appendChild(small);
 		}
