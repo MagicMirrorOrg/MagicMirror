840315e99b802f229e7902043046cfc220b97cb6
bemanuel <bemanuel.pe@gmail.com>
2018-10-05 17:44:21 -0300

    Adding capacity to show image from rss
    Permit to show images from rss.

--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -21,6 +21,7 @@ Module.register("newsfeed",{
 		showSourceTitle: true,
 		showPublishDate: true,
 		showDescription: false,
+		showImage: false,
 		wrapTitle: true,
 		wrapDescription: true,
 		truncDescription: true,
@@ -99,6 +100,17 @@ Module.register("newsfeed",{
 		}
 
 		if (this.newsItems.length > 0) {
+
+                        if (this.config.showImage) {
+                                var image = document.createElement("img");
+
+                                if (this.newsItems[this.activeItem].media !="") {
+                                        image.src = this.newsItems[this.activeItem].media;
+                                } else {
+                                        image.src = this.config.defaultImage;
+                                }
+                                wrapper.appendChild(image)
+                        }
 
 			// this.config.showFullArticle is a run-time configuration, triggered by optional notifications
 			if (!this.config.showFullArticle && (this.config.showSourceTitle || this.config.showPublishDate)) {
