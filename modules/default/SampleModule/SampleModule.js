7229b40bfe28e4bf5558960b0c3081684ff523ac
sam detweiler <sdetweil@gmail.com>
2019-03-10 21:42:31 -0500

    fix tabs and spaces

--- modules/default/SampleModule/SampleModule.js
+++ modules/default/SampleModule/SampleModule.js
@@ -1,122 +1,120 @@
 /*
 
 sample module structure
- 
- 
- */
- 
- 
-Module.register('SampleModule', {
-   some_variable:  true,
-   some_other_valriable: 'a string',  
-   defaults: {
-     // holder for config info from module_name.js
-     config:null,
-   },
-   
-   init: function(){
-    Log.log(this.name + ' is in init!');
-   },
-  
-   start: function(){
-    Log.log(this.name + ' is starting!');
-   },
-   
-   loaded: function(callback) {
-    Log.log(this.name + ' is loaded!');
-    callback();
-   },
-   
-   // return list of other functional scripts to use, if any (like require in node_helper)
-   getScripts: function() {
-     return 
-      [  
-        // sample of list of files to specify here, if no files,do not use this routine, or return empty list
-        
-        //'script.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-        //'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-        //this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
-        //'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-      ]
-    }, 
-   
-   // return list of stylesheet files to use if any
-   getStyles: function() {
-    return 
-     [ 
-      // sample of list of files to specify here, if no files, do not use this routine, , or return empty list
-      
-      //'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-      //'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
-      //this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
-      //'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
-     ]
-   }, 
-      
-     // return list of translation files to use, if any
-   getTranslations: function() {
-    return 
-     [ 
-       // sample of list of files to specify here, if no files, do not use this routine, , or return empty list
-       
-       // en: "translations/en.json",  (folders and filenames in your module folder)
-       // de: "translations/de.json"
-     ]
-   }, 
-  
-  // this is the major worker of the module, it provides the displayable content for this module
-  getDom: function() {
-    var wrapper = document.createElement("div");
-   
-    // if user supplied message text in its module config, use it
-    if(this.config.hasOwnProperty('message'))
-      // using text from module config block in config.js
-      wrapper.innerHTML = this.config.message;
-    else
-      // use hard coded text
-      wrapper.innerHTML = 'Hello world!';
-    
-    // pass the created content back to MM to add to DOM.    
-    return wrapper;
-  },
-  
-  // only called if the module header was configured in module config in config.js
-  getHeader: function() {
-    return this.data.header + ' Foo Bar';
-  },
-  
-  // messages received from other modules and the system (NOT from your node helper)
-  // payload is a notification dependent data structure
-  notificationReceived: function(notification, payload, sender) {
-    // once everybody is loaded up
-    if(notification==='ALL_MODULES_STARTED'){
-      // send our config to the node_helper
-      this.sendSocketNotification('CONFIG',this.config)
-    }
-    if (sender) {
-      Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-    } else {
-      Log.log(this.name + " received a system notification: " + notification);
-    }
-  },
-  // messages received from from your node helper (NOT other modules or the system) 
-  // payload is a notification dependent data structure, up to you to design between module and node_helper
-  socketNotificationReceived: function(notification, payload) {
-    Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
-  },
-  
-  // system notification your module is being hidden
-  // typically you would stop doing UI updates (getDom/updateDom) if the module is hidden
-  suspend: function(){
-    
-  },
-  
-    // system notification your module is being unhidden/shown
-    // typically you would resume doing UI updates (getDom/updateDom) if the module is shown
-  resume: function(){
-    
-  },
 
-  
-  
+
+ */
+
+
+Module.register("SampleModule", {
+	some_variable:  true,
+	some_other_valriable: "a string",
+	defaults: {
+		// holder for config info from module_name.js
+		config:null,
+	},
+
+	init: function(){
+		Log.log(this.name + " is in init!");
+	},
+
+	start: function(){
+		Log.log(this.name + " is starting!");
+	},
+
+	loaded: function(callback) {
+		Log.log(this.name + " is loaded!");
+		callback();
+	},
+
+	// return list of other functional scripts to use, if any (like require in node_helper)
+	getScripts: function() {
+		return
+		[
+			// sample of list of files to specify here, if no files,do not use this routine, or return empty list
+
+			//'script.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+			//'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
+			//this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
+			//'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
+		]
+	},
+
+	// return list of stylesheet files to use if any
+	getStyles: function() {
+		return
+		[
+			// sample of list of files to specify here, if no files, do not use this routine, , or return empty list
+
+			//'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+			//'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
+			//this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
+			//'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
+		]
+	},
+
+	// return list of translation files to use, if any
+	getTranslations: function() {
+		return
+		[
+			// sample of list of files to specify here, if no files, do not use this routine, , or return empty list
+
+			// en: "translations/en.json",  (folders and filenames in your module folder)
+			// de: "translations/de.json"
+		]
+	},
+
+	// this is the major worker of the module, it provides the displayable content for this module
+	getDom: function() {
+		var wrapper = document.createElement("div");
+
+		// if user supplied message text in its module config, use it
+		if(this.config.hasOwnProperty("message"))
+		// using text from module config block in config.js
+		{wrapper.innerHTML = this.config.message;}
+		else
+		// use hard coded text
+		{wrapper.innerHTML = "Hello world!";}
+
+		// pass the created content back to MM to add to DOM.
+		return wrapper;
+	},
+
+	// only called if the module header was configured in module config in config.js
+	getHeader: function() {
+		return this.data.header + " Foo Bar";
+	},
+
+	// messages received from other modules and the system (NOT from your node helper)
+	// payload is a notification dependent data structure
+	notificationReceived: function(notification, payload, sender) {
+		// once everybody is loaded up
+		if(notification==="ALL_MODULES_STARTED"){
+			// send our config to the node_helper
+			this.sendSocketNotification("CONFIG",this.config)
+		}
+		if (sender) {
+			Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+		} else {
+			Log.log(this.name + " received a system notification: " + notification);
+		}
+	},
+	// messages received from from your node helper (NOT other modules or the system)
+	// payload is a notification dependent data structure, up to you to design between module and node_helper
+	socketNotificationReceived: function(notification, payload) {
+		Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+	},
+
+	// system notification your module is being hidden
+	// typically you would stop doing UI updates (getDom/updateDom) if the module is hidden
+	suspend: function(){
+
+	},
+
+	// system notification your module is being unhidden/shown
+	// typically you would resume doing UI updates (getDom/updateDom) if the module is shown
+	resume: function(){
+
+	},
+
 })
--- modules/default/SampleModule/node_helper.js
+++ modules/default/SampleModule/node_helper.js
@@ -4,31 +4,31 @@ var NodeHelper = require("node_helper");
 // var xxx = require('yyy') here
 
 module.exports = NodeHelper.create({
-  
-    init(){
-        console.log(`init module helper SampleModule`);
-    },
-    
-    start() {
-        console.log(`Starting module helper: ${this.name}`);
-    },
-    
-    stop(){
-        console.log(`Stopping module helper: ${this.name}`);
-    },
 
-   // handle messages from our module// each notification indicates a different messages
-   // payload is a data structure that is different per message.. up to you to design this 
-   socketNotificationReceived(notification, payload) {
-        console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
-        // if config message from module
-        if (notification === 'CONFIG') {
-          // save payload config info
-          this.config=payload
-        }
-        else if(notification === '????2') {
-        }
-        
-   },
-    
+	init(){
+		console.log("init module helper SampleModule");
+	},
+
+	start() {
+		console.log(`Starting module helper: ${this.name}`);
+	},
+
+	stop(){
+		console.log(`Stopping module helper: ${this.name}`);
+	},
+
+	// handle messages from our module// each notification indicates a different messages
+	// payload is a data structure that is different per message.. up to you to design this
+	socketNotificationReceived(notification, payload) {
+		console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		// if config message from module
+		if (notification === "CONFIG") {
+			// save payload config info
+			this.config=payload
+		}
+		else if(notification === "????2") {
+		}
+
+	},
+
 });
