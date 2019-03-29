979041ee91e0690f0dfacb7000a1ab02413c427d
Chris van Marle <qistoph@gmail.com>
2019-03-29 13:13:56 +0100

    Fix typo in variable name fetchedLocationName

--- modules/default/currentweather/currentweather.js
+++ modules/default/currentweather/currentweather.js
@@ -71,7 +71,7 @@ Module.register("currentweather",{
 	firstEvent: false,
 
 	// create a variable to hold the location name based on the API result.
-	fetchedLocatioName: "",
+	fetchedLocationName: "",
 
 	// Define required scripts.
 	getScripts: function() {
@@ -266,7 +266,7 @@ Module.register("currentweather",{
 	// Override getHeader method.
 	getHeader: function() {
 		if (this.config.appendLocationNameToHeader) {
-			return this.data.header + " " + this.fetchedLocatioName;
+			return this.data.header + " " + this.fetchedLocationName;
 		}
 
 		return this.data.header;
