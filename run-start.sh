55df862186a81951b04779490c227fa0572de86f
Martin van Es <martin@mrvanes.com>
2019-07-18 18:04:56 +0200

    Untrack custom.css

--- run-start.sh
+++ run-start.sh
@@ -1,4 +1,7 @@
 if [ -z "$DISPLAY" ]; then #If not set DISPLAY is SSH remote or tty
 	export DISPLAY=:0 # Set by default display
 fi
+if [ ! -f css/custom.css ]; then
+  cp css/custom-example.css css/custom.css
+fi
 electron js/electron.js $1
