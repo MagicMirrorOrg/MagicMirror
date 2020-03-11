0b2e836e3d39ebf8a05ab1b2ffae7654bd31385c
rejas <github@veeck.de>
2020-03-11 09:01:21 +0100

    Commit a faulty js to see if travis complains

--- modules/default/defaultmodules.js
+++ modules/default/defaultmodules.js
@@ -8,7 +8,7 @@
 // Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
 
 var defaultModules = [
-	"alert",
+	'alert',
 	"calendar",
 	"clock",
 	"compliments",
