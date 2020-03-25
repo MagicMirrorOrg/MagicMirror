4e4d3418b365b1c639a344f7512e9eaea0a92ec6
rejas <github@veeck.de>
2020-03-25 06:53:09 +0100

    LInt mockdate helper

--- tests/e2e/modules/mocks/date.js
+++ tests/e2e/modules/mocks/date.js
@@ -6,88 +6,88 @@
 */
 
 (function(name, definition) {
-    if (typeof module !== 'undefined') module.exports = definition();
-    else if (typeof define === 'function' && typeof define.amd === 'object') define(definition);
-    else this[name] = definition();
-}('MockDate', function() {
-    "use strict";
+	if (typeof module !== "undefined") {module.exports = definition();}
+	else if (typeof define === "function" && typeof define.amd === "object") {define(definition);}
+	else {this[name] = definition();}
+}("MockDate", function() {
+	"use strict";
 
-    var _Date = Date
-        , _getTimezoneOffset = Date.prototype.getTimezoneOffset
-        , now   = null
+	var _Date = Date
+		, _getTimezoneOffset = Date.prototype.getTimezoneOffset
+		, now = null
     ;
 
-    function MockDate(y, m, d, h, M, s, ms) {
-        var date;
+	function MockDate(y, m, d, h, M, s, ms) {
+		var date;
 
-        switch (arguments.length) {
+		switch (arguments.length) {
 
-            case 0:
-                if (now !== null) {
-                    date = new _Date(now);
-                } else {
-                    date = new _Date();
-                }
-                break;
+		case 0:
+			if (now !== null) {
+				date = new _Date(now);
+			} else {
+				date = new _Date();
+			}
+			break;
 
-            case 1:
-                date = new _Date(y);
-                break;
+		case 1:
+			date = new _Date(y);
+			break;
 
-            default:
-                d = typeof d === 'undefined' ? 1 : d;
-                h = h || 0;
-                M = M || 0;
-                s = s || 0;
-                ms = ms || 0;
-                date = new _Date(y, m, d, h, M, s, ms);
-                break;
-        }
+		default:
+			d = typeof d === "undefined" ? 1 : d;
+			h = h || 0;
+			M = M || 0;
+			s = s || 0;
+			ms = ms || 0;
+			date = new _Date(y, m, d, h, M, s, ms);
+			break;
+		}
 
-        return date;
-    }
+		return date;
+	}
 
-    MockDate.UTC = _Date.UTC;
+	MockDate.UTC = _Date.UTC;
 
-    MockDate.now = function() {
-        return new MockDate().valueOf();
-    };
+	MockDate.now = function() {
+		return new MockDate().valueOf();
+	};
 
-    MockDate.parse = function(dateString) {
-        return _Date.parse(dateString);
-    };
+	MockDate.parse = function(dateString) {
+		return _Date.parse(dateString);
+	};
 
-    MockDate.toString = function() {
-        return _Date.toString();
-    };
+	MockDate.toString = function() {
+		return _Date.toString();
+	};
 
-    MockDate.prototype = _Date.prototype;
+	MockDate.prototype = _Date.prototype;
 
-    function set(date, timezoneOffset) {
-        var dateObj = new Date(date)
-        if (isNaN(dateObj.getTime())) {
-            throw new TypeError('mockdate: The time set is an invalid date: ' + date)
-        }
+	function set(date, timezoneOffset) {
+		var dateObj = new Date(date);
+		if (isNaN(dateObj.getTime())) {
+			throw new TypeError("mockdate: The time set is an invalid date: " + date);
+		}
 
-        if (typeof timezoneOffset === 'number') {
-            MockDate.prototype.getTimezoneOffset = function() {
-                return timezoneOffset;
-            }
-        }
+		if (typeof timezoneOffset === "number") {
+			MockDate.prototype.getTimezoneOffset = function() {
+				return timezoneOffset;
+			};
+		}
 
-        Date = MockDate;
+		Date = MockDate;
 
-        now = dateObj.valueOf();
-    }
+		now = dateObj.valueOf();
+	}
 
-    function reset() {
-        Date = _Date;
-        Date.prototype.getTimezoneOffset = _getTimezoneOffset
-    }
+	function reset() {
+		Date = _Date;
+		Date.prototype.getTimezoneOffset = _getTimezoneOffset;
+	}
 
-    return {
-        set: set,
-        reset: reset
-    };
+	return {
+		set: set,
+		reset: reset
+	};
 
 }));
