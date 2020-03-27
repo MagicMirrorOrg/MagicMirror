cb793fcdc68797955e3c291cf3754a6b8a358c93
AndreKoepke <andre.koepke@haw-hamburg.de>
2020-03-27 21:30:27 +0100

    Update electron.js
    use tls if it was configured

--- js/electron.js
+++ js/electron.js
@@ -48,7 +48,14 @@ function createWindow() {
 	// and load the index.html of the app.
 	// If config.address is not defined or is an empty string (listening on all interfaces), connect to localhost
 	var address = (config.address === void 0) | (config.address === "") ? (config.address = "localhost") : config.address;
-	mainWindow.loadURL(`http://${address}:${config.port}`);
+	
+	if (!config["tls] !== null && config.tls) {
+		var prefix = 'https://';
+	} else {
+		var prefix = 'http://';
+	}
+	
+	mainWindow.loadURL(`${prefix}${address}:${config.port}`);
 
 	// Open the DevTools if run with "npm start dev"
 	if (process.argv.includes("dev")) {
