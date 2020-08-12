70682bb0dd9d2774813fcabc0793ad95a59a1c72
bugsounet <bugsounet@bugsnana.fr>
2020-08-12 18:01:39 +0200

    configMerge issue

--- js/module.js
+++ js/module.js
@@ -226,7 +226,7 @@ var Module = Class.extend({
 	 * argument config object - Module config.
 	 */
 	setConfig: function (config) {
-		this.config = Object.assign({}, this.defaults, config);
+		this.config = configMerge({}, this.defaults, config);
 	},
 
 	/* socket()
@@ -435,6 +435,46 @@ var Module = Class.extend({
 });
 
 Module.definitions = {};
+
+/** Merging MagicMirror (or other) default/config script **/
+/** merge 2 objects or/with array **/
+/** using: **/
+/** ------- **/
+/** this.config = configMerge({}, this.defaults, this.config) **/
+/** ------- **/
+/** arg1: initial objet **/
+/** arg2: config model **/
+/** arg3: config to merge **/
+/** ------- **/
+/** why using it ? **/
+/** Object.assign() function don't to all job **/
+/** it don't merge all thing in deep **/
+/** -> object in object and array is not merging **/
+/** ------- **/
+/** @bugsounet **/
+
+function configMerge(result) {
+	var stack = Array.prototype.slice.call(arguments, 1);
+	var item;
+	var key;
+	while (stack.length) {
+		item = stack.shift();
+		for (key in item) {
+			if (item.hasOwnProperty(key)) {
+				if (typeof result[key] === "object" && result[key] && Object.prototype.toString.call(result[key]) !== "[object Array]") {
+					if (typeof item[key] === "object" && item[key] !== null) {
+						result[key] = configMerge({}, result[key], item[key]);
+					} else {
+						result[key] = item[key];
+					}
+				} else {
+					result[key] = item[key];
+				}
+			}
+		}
+	}
+	return result;
+}
 
 Module.create = function (name) {
 	// Make sure module definition is available.
