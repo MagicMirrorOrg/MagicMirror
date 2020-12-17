b373aa62503d9a610019157c4100e5a15b4566dc
veeck <veeck@neofonie.de>
2020-12-17 18:31:18 +0100

    Hide alert overlay when dismissed manually

--- modules/default/alert/alert.js
+++ modules/default/alert/alert.js
@@ -100,6 +100,7 @@ Module.register("alert", {
 			message: image + message,
 			effect: this.config.alert_effect,
 			ttl: params.timer,
+			onClose: () => this.hide_alert(sender),
 			al_no: "ns-alert"
 		});
 		//Show alert
