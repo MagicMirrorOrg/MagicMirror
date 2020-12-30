e51f6597ed8a068b147bdb8d1055de6d80a87a94
drewski3420@gmail.com <drewski3420@gmail.com>
2020-12-30 09:03:19 -0500

    This change prevents returning '-0' (negative zero) when roundTemp is true.

--- modules/default/weather/weather.js
+++ modules/default/weather/weather.js
@@ -181,7 +181,7 @@ Module.register("weather", {
 
 	roundValue: function (temperature) {
 		var decimals = this.config.roundTemp ? 0 : 1;
-		return parseFloat(temperature).toFixed(decimals);
+		return parseFloat(Math.abs(temperature) < 0.5 && this.config.roundTemp ?  0 : temperature).toFixed(decimals);
 	},
 
 	addFilters() {
