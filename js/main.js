41da6f455ab296740c521a1960cb777a92362bb7
Felix Wiedenbach <felix.wiedenbach@fleetster.net>
2021-01-28 21:23:48 +0100

    use error object for callback to include stack trace

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@ var MM = (function () {
 		// Otherwise cancel show action.
 		if (module.lockStrings.length !== 0 && options.force !== true) {
 			Log.log("Will not show " + module.name + ". LockStrings active: " + module.lockStrings.join(","));
-			callback("ERR_ACTIVE_LOCK_STRINGS");
+			callback(new Error("ERR_ACTIVE_LOCK_STRINGS"));
 			return;
 		}
 
