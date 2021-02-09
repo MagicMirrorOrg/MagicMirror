120f0299b2a7fc08100a393f9338bf13bf591ba5
TheDuffman85 <benjamin.tausend@googlemail.com>
2021-02-09 16:05:38 +0100

    Added a new parameter to hide time portion on relative times
    Added a new parameter hideTime with default value false. This parameter hides the time portion on relative times.

--- modules/default/calendar/calendar.js
+++ modules/default/calendar/calendar.js
@@ -36,6 +36,7 @@ Module.register("calendar", {
 		fadePoint: 0.25, // Start on 1/4th of the list.
 		hidePrivate: false,
 		hideOngoing: false,
+		hideTime: false,
 		colored: false,
 		coloredSymbolOnly: false,
 		customEvents: [], // Array of {keyword: "", symbol: "", color: ""} where Keyword is a regexp and symbol/color are to be applied for matched
@@ -366,7 +367,17 @@ Module.register("calendar", {
 					// Show relative times
 					if (event.startDate >= now) {
 						// Use relative  time
-						timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").calendar());
+						if (!this.config.hideTime) {
+							timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").calendar());
+						} else {
+							timeWrapper.innerHTML = this.capFirst(
+								moment(event.startDate, "x").calendar(null, {
+									sameDay: "[Today]",
+									nextDay: "[Tomorrow]",
+									nextWeek: "dddd"
+								})
+							);
+						}
 						if (event.startDate - now < this.config.getRelative * oneHour) {
 							// If event is within getRelative  hours, display 'in xxx' time format or moment.fromNow()
 							timeWrapper.innerHTML = this.capFirst(moment(event.startDate, "x").fromNow());
