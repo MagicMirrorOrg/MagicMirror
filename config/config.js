105b1d58a2407f37356fee8133ab998591d19ece
Nishant Mehta <nishmeht7@gmail.com>
2021-02-21 15:22:24 +0400

    fixed default config

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@
  */
 
 var config = {
-	address: "localhost", 	// Address to listen on, can be:
+	address: "0.0.0.0", 	// Address to listen on, can be:
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
@@ -17,7 +17,7 @@ var config = {
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+	ipWhitelist: [], 	// Set [] to allow all IP addresses
 															// or add a specific IPv4 of 192.168.1.5 :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -99,6 +99,16 @@ var config = {
 				broadcastNewsFeeds: true,
 				broadcastNewsUpdates: true
 			}
+		},
+		{
+			module: 'MMM-Remote-Control',
+			config: {
+				customCommand: {},
+				customMenu: 'custom_menu.json',
+				showModuleApiMenu: true,
+				apiKey: ''
+			},
+			hidden: false
 		},
 	]
 };
