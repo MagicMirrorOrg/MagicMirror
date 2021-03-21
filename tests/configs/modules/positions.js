dd366f35a88583fcb198b7eb102c9a3f57972ddf
rejas <github@veeck.de>
2021-03-21 17:20:31 +0100

    Use es6 notation in test configs

--- tests/configs/modules/positions.js
+++ tests/configs/modules/positions.js
@@ -5,8 +5,7 @@
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-
-var config = {
+let config = {
 	port: 8080,
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
 
@@ -23,9 +22,9 @@ var config = {
 	modules:
 		// Using exotic content. This is why don't accept go to JSON configuration file
 		(function () {
-			var positions = ["top_bar", "top_left", "top_center", "top_right", "upper_third", "middle_center", "lower_third", "bottom_left", "bottom_center", "bottom_right", "bottom_bar", "fullscreen_above", "fullscreen_below"];
-			var modules = Array();
-			for (var idx in positions) {
+			let positions = ["top_bar", "top_left", "top_center", "top_right", "upper_third", "middle_center", "lower_third", "bottom_left", "bottom_center", "bottom_right", "bottom_bar", "fullscreen_above", "fullscreen_below"];
+			let modules = Array();
+			for (let idx in positions) {
 				modules.push({
 					module: "helloworld",
 					position: positions[idx],
