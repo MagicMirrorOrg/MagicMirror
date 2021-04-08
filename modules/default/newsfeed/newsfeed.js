a49962b8de73833aedc033fcce1ec5ff1c3ca5b2
oemel09 <github@oemel09.de>
2021-04-08 16:48:29 +0200

    Moves validation into template

--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -135,7 +135,7 @@ Module.register("newsfeed", {
 			sourceTitle: item.sourceTitle,
 			publishDate: moment(new Date(item.pubdate)).fromNow(),
 			title: item.title,
-			description: this.isShowingDescription ? item.description : null
+			description: item.description
 		};
 	},
 
