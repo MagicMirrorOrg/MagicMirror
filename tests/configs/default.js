179989aa4237b3babc87c727891d20c7611008d3
Karsten Hassel <hassel@gmx.de>
2021-07-05 19:21:39 +0200

    add missing comments

--- tests/configs/default.js
+++ tests/configs/default.js
@@ -1,3 +1,8 @@
+/* Magic Mirror Test default config for modules
+ *
+ * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
+ * MIT Licensed.
+ */
 let config = {
 	port: 8080,
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
@@ -16,6 +21,7 @@ let config = {
 	modules: []
 };
 
+/*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
 	module.exports = config;
 }
