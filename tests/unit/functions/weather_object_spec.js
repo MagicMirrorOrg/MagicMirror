087a4727657449be8cfaf348cf1dc22d2c8d8261
rejas <github@veeck.de>
2021-09-06 21:15:23 +0200

    Fix tomezone test error as reported by @khassel

--- tests/unit/functions/weather_object_spec.js
+++ tests/unit/functions/weather_object_spec.js
@@ -1,12 +1,15 @@
 const WeatherObject = require("../../../modules/default/weather/weatherobject.js");
 
-global.moment = require("moment");
+global.moment = require("moment-timezone");
 global.SunCalc = require("suncalc");
 
 describe("WeatherObject", function () {
+	let originalTimeZone;
 	let weatherobject;
 
 	beforeAll(function () {
+		originalTimeZone = moment.tz.guess();
+		moment.tz.setDefault("Africa/Dar_es_Salaam");
 		weatherobject = new WeatherObject("metric", "metric", "metric", true);
 	});
 
@@ -20,5 +23,9 @@ describe("WeatherObject", function () {
 		weatherobject.date = moment(0, "HH");
 		weatherobject.updateSunTime(-6.774877582342688, 37.63345667023327);
 		expect(weatherobject.isDayTime()).toBe(false);
+	});
+
+	afterAll(function () {
+		moment.tz.setDefault(originalTimeZone);
 	});
 });
