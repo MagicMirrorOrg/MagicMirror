53c789bff946334eb94a996af61a11fca33afe8a
Karsten Hassel <hassel@gmx.de>
2021-09-09 21:49:10 +0200

    again logger.js due to problem with e2e test

--- js/logger.js
+++ js/logger.js
@@ -9,13 +9,11 @@
  */
 (function (root, factory) {
 	if (typeof exports === "object") {
-		if (process.env.JEST_WORKER_ID === undefined) {
-			// add timestamps in front of log messages
-			require("console-stamp")(console, {
-				pattern: "yyyy-mm-dd HH:MM:ss.l",
-				include: ["debug", "log", "info", "warn", "error"]
-			});
-		}
+		// add timestamps in front of log messages
+		require("console-stamp")(console, {
+			pattern: "yyyy-mm-dd HH:MM:ss.l",
+			include: ["debug", "log", "info", "warn", "error"]
+		});
 
 		// Node, CommonJS-like
 		module.exports = factory(root.config);
@@ -39,31 +37,30 @@
 			timeEnd: Function.prototype.bind.call(console.timeEnd, console),
 			timeStamp: Function.prototype.bind.call(console.timeStamp, console)
 		};
-
-		logLevel.setLogLevel = function (newLevel) {
-			if (newLevel) {
-				Object.keys(logLevel).forEach(function (key, index) {
-					if (!newLevel.includes(key.toLocaleUpperCase())) {
-						logLevel[key] = function () {};
-					}
-				});
-			}
-		};
 	} else {
 		logLevel = {
-			debug: function () {},
-			log: function () {},
-			info: function () {},
-			warn: function () {},
-			error: function () {},
-			group: function () {},
-			groupCollapsed: function () {},
-			groupEnd: function () {},
-			time: function () {},
-			timeEnd: function () {},
-			timeStamp: function () {}
+			debug: Function.prototype.bind.call(console.debug, console),
+			log: Function.prototype.bind.call(console.log, console),
+			info: Function.prototype.bind.call(console.info, console),
+			warn: Function.prototype.bind.call(console.warn, console),
+			error: Function.prototype.bind.call(console.error, console),
+			group: Function.prototype.bind.call(console.group, console),
+			groupCollapsed: Function.prototype.bind.call(console.groupCollapsed, console),
+			groupEnd: Function.prototype.bind.call(console.groupEnd, console),
+			time: Function.prototype.bind.call(console.time, console),
+			timeEnd: Function.prototype.bind.call(console.timeEnd, console)
 		};
 	}
+
+	logLevel.setLogLevel = function (newLevel) {
+		if (newLevel) {
+			Object.keys(logLevel).forEach(function (key, index) {
+				if (!newLevel.includes(key.toLocaleUpperCase())) {
+					logLevel[key] = function () {};
+				}
+			});
+		}
+	};
 
 	return logLevel;
 });
