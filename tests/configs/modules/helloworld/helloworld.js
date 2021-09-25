e0906f3462b34e70fc3ae6d7f763e387484ac3d9
Karsten Hassel <hassel@gmx.de>
2021-09-25 23:51:32 +0200

    update module helloworld

--- tests/configs/modules/helloworld/helloworld.js
+++ tests/configs/modules/helloworld/helloworld.js
@@ -3,7 +3,13 @@
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-let config = require(process.cwd() + "/tests/configs/default.js").configFactory({
+let config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
+
 	modules: [
 		{
 			module: "helloworld",
@@ -13,7 +19,7 @@ let config = require(process.cwd() + "/tests/configs/default.js").configFactory(
 			}
 		}
 	]
-});
+};
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
--- tests/configs/modules/helloworld/helloworld_default.js
+++ tests/configs/modules/helloworld/helloworld_default.js
@@ -3,14 +3,20 @@
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-let config = require(process.cwd() + "/tests/configs/default.js").configFactory({
+let config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
+
 	modules: [
 		{
 			module: "helloworld",
 			position: "bottom_bar"
 		}
 	]
-});
+};
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
