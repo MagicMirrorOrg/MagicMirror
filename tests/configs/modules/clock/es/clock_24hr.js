f750436b64c54a090767d2c88a36e87da3138356
Karsten Hassel <hassel@gmx.de>
2021-09-26 22:09:41 +0200

    update module clock_es

--- tests/configs/modules/clock/es/clock_24hr.js
+++ tests/configs/modules/clock/es/clock_24hr.js
@@ -3,8 +3,12 @@
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-let config = require(process.cwd() + "/tests/configs/default.js").configFactory({
+let config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
 	language: "es",
+	timeFormat: 24,
+	units: "metric",
 
 	modules: [
 		{
@@ -12,7 +16,7 @@ let config = require(process.cwd() + "/tests/configs/default.js").configFactory(
 			position: "middle_center"
 		}
 	]
-});
+};
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
