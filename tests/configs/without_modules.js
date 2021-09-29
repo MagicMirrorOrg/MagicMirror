b3bddb2c990cc0adfd64851428781ba3f4691daa
Karsten Hassel <hassel@gmx.de>
2021-09-29 21:59:22 +0200

    fix without_modules.js

--- tests/configs/without_modules.js
+++ tests/configs/without_modules.js
@@ -4,9 +4,7 @@
  * MIT Licensed.
  */
 let config = {
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.10.1"],
-
-	units: "metric"
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.10.1"]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
