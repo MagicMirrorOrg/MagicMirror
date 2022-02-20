e234d2379bd2745e277b136ec121880c5e481ebb
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-20 02:24:22 +0100

    Fix log output
    Before:
    ```
    MMM-OlympicGames - Load translationfalse: translations/de.json
    translator.js:107 MMM-OlympicGames - Load translation  fallback: translations/en.json
    ```
    
    After:
    ```
    MMM-OlympicGames - Load translation: translations/de.json
    translator.js:107 MMM-OlympicGames - Load translation fallback: translations/en.json
    ```

--- js/translator.js
+++ js/translator.js
@@ -104,7 +104,7 @@ const Translator = (function () {
 		 * @param {Function} callback Function called when done.
 		 */
 		load(module, file, isFallback, callback) {
-			Log.log(`${module.name} - Load translation${isFallback && " fallback"}: ${file}`);
+			Log.log(`${module.name} - Load translation${isFallback ? " fallback" : ""}: ${file}`);
 
 			if (this.translationsFallback[module.name]) {
 				callback();
