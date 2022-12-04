24cdb3c8f782d964f008c91925c21204c384883e
Eldar <eldarhasanovic@eldars-mbp.lan>
2022-12-03 20:22:14 -0600

    commit

---
+++ js/eldymain.js
@@ -0,0 +1,22 @@
+const padTwoZeros = (n) => String(n).padStart(2, "0");
+
+const clockUpdate = () => {
+	document.getElementById("clockDate").textContent = new Date(Date.now()).toDateString();
+
+	const currentTime = new Date(Date.now());
+	const h = padTwoZeros(currentTime.getHours());
+	const m = padTwoZeros(currentTime.getMinutes());
+	const s = padTwoZeros(currentTime.getSeconds());
+
+	const currentTimeString = `${h}:${m}:${s}`;
+
+	document.getElementById("clockTime").textContent = currentTimeString;
+};
+
+const eldyMirrorRunner = () => {
+	console.log("hello world");
+
+	setInterval(clockUpdate, 1000);
+};
+
+eldyMirrorRunner();
