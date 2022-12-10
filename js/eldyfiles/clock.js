9cf97d0e6c448a36cad71714697a2952f2cebf9c
Eldar Hasanovic <you@example.com>
2022-12-09 19:25:10 -0600

    commit

---
+++ js/eldyfiles/clock.js
@@ -0,0 +1,14 @@
+export const clockUpdate = () => {
+	document.getElementById("clockDate").textContent = new Date(Date.now()).toDateString();
+
+	const currentTime = new Date(Date.now());
+	const [h, m, s] = [currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds()].map(padTwoZeros);
+
+	const hourModulo = h % 12;
+
+	const currentTimeString12hr = `${hourModulo === 0 ? 12 : hourModulo}:${m}${h >= 12 ? "pm" : "am"}`;
+	const currentTimeString24hr = `${h}:${m}:${s}`;
+
+	document.getElementById("clockTime12hr").textContent = currentTimeString12hr;
+	document.getElementById("clockTime24hr").textContent = currentTimeString24hr;
+};
