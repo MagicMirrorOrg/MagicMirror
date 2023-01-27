684c727430586d88a48202f9276f63ed0ae3decc
Magnus Marthinsen <magmar@online.no>
2023-01-27 21:30:54 +0100

    Updated ukmetoffice to be more consistent with the WeatherObject datamodel.

--- modules/default/weather/providers/ukmetoffice.js
+++ modules/default/weather/providers/ukmetoffice.js
@@ -100,7 +100,7 @@ WeatherProvider.register("ukmetoffice", {
 							currentWeather.humidity = rep.H;
 							currentWeather.temperature = rep.T;
 							currentWeather.feelsLikeTemp = rep.F;
-							currentWeather.precipitationAmount = parseInt(rep.Pp);
+							currentWeather.precipitationProbability = parseInt(rep.Pp);
 							currentWeather.windSpeed = WeatherUtils.convertWindToMetric(rep.S);
 							currentWeather.windFromDirection = WeatherUtils.convertWindDirection(rep.D);
 							currentWeather.weatherType = this.convertWeatherType(rep.W);
@@ -138,7 +138,7 @@ WeatherProvider.register("ukmetoffice", {
 				weather.minTemperature = period.Rep[1].Nm;
 				weather.maxTemperature = period.Rep[0].Dm;
 				weather.weatherType = this.convertWeatherType(period.Rep[0].W);
-				weather.precipitationAmount = parseInt(period.Rep[0].PPd);
+				weather.precipitationProbability = parseInt(period.Rep[0].PPd);
 
 				days.push(weather);
 			}
