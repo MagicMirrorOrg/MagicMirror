498b44017491b515f1badd346017300a1dceac5c
Veeck <github@veeck.de>
2023-02-22 18:58:29 +0100

    Convert module-start to async (#3049)
    Similar to the node_helper async start PR...
    
    ---------
    
    Co-authored-by: veeck <michael@veeck.de>

--- modules/default/alert/alert.js
+++ modules/default/alert/alert.js
@@ -44,7 +44,7 @@ Module.register("alert", {
 		return `templates/${type}.njk`;
 	},
 
-	start() {
+	async start() {
 		Log.info(`Starting module: ${this.name}`);
 
 		if (this.config.effect === "slide") {
@@ -53,7 +53,7 @@ Module.register("alert", {
 
 		if (this.config.welcome_message) {
 			const message = this.config.welcome_message === true ? this.translate("welcome") : this.config.welcome_message;
-			this.showNotification({ title: this.translate("sysTitle"), message });
+			await this.showNotification({ title: this.translate("sysTitle"), message });
 		}
 	},
 
