MagicMirrorOrg/MagicMirror

commit 5f38c532600a257e7c92079e58014e23bf21d787

Revise require imports (#3071)
- order (external first)
- remove superfluous file extensions
- new line after imports
- deconstruct (only one time (in `check_config.js`))
- fix path (only one time (in `global-setup.js`))

serveronly/index.js
@@ -1,5 +1,5 @@
-const app = require("../js/app.js");
 const Log = require("logger");
+const app = require("../js/app");
 
 app.start().then((config) => {
 	const bindAddress = config.address ? config.address : "localhost";

js/app.js
@@ -10,11 +10,11 @@ require("module-alias/register");
 
 const fs = require("fs");
 const path = require("path");
+const envsub = require("envsub");
 const Log = require("logger");
 const Server = require(`${__dirname}/server`);
 const Utils = require(`${__dirname}/utils`);
 const defaultModules = require(`${__dirname}/../modules/default/defaultmodules`);
-const envsub = require("envsub");
 
 // Get version number.
 global.version = require(`${__dirname}/../package.json`).version;

js/check_config.js
@@ -5,11 +5,11 @@
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-const Linter = require("eslint").Linter;
-const linter = new Linter();
-
 const path = require("path");
 const fs = require("fs");
+const { Linter } = require("eslint");
+
+const linter = new Linter();
 
 const rootPath = path.resolve(`${__dirname}/../`);
 const Log = require(`${rootPath}/js/logger.js`);

js/electron.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const electron = require("electron");
-const core = require("./app.js");
-const Log = require("logger");
+const core = require("./app");
+const Log = require("./logger");
 
 // Config
 let config = process.env.config ? JSON.parse(process.env.config) : {};

js/node_helper.js
@@ -4,9 +4,9 @@
  * By Michael Teeuw https://michaelteeuw.nl
  * MIT Licensed.
  */
-const Class = require("./class.js");
-const Log = require("logger");
 const express = require("express");
+const Log = require("logger");
+const Class = require("./class");
 
 const NodeHelper = Class.extend({
 	init() {

js/server.js
@@ -4,18 +4,18 @@
  * By Michael Teeuw https://michaelteeuw.nl
  * MIT Licensed.
  */
-const express = require("express");
-const path = require("path");
-const ipfilter = require("express-ipfilter").IpFilter;
 const fs = require("fs");
-const helmet = require("helmet");
-const socketio = require("socket.io");
 const http = require("http");
 const https = require("https");
+const path = require("path");
+const express = require("express");
+const ipfilter = require("express-ipfilter").IpFilter;
+const helmet = require("helmet");
+const socketio = require("socket.io");
 
 const Log = require("logger");
-const Utils = require("./utils.js");
-const { cors, getConfig, getHtml, getVersion } = require("./server_functions.js");
+const Utils = require("./utils");
+const { cors, getConfig, getHtml, getVersion } = require("./server_functions");
 
 /**
  * Server

js/server_functions.js
@@ -1,7 +1,7 @@
-const fetch = require("./fetch");
 const fs = require("fs");
 const path = require("path");
 const Log = require("logger");
+const fetch = require("./fetch");
 
 /**
  * Gets the config.

modules/default/calendar/calendarfetcher.js
@@ -4,13 +4,14 @@
  * By Michael Teeuw https://michaelteeuw.nl
  * MIT Licensed.
  */
-const CalendarUtils = require("./calendarutils");
-const Log = require("logger");
-const NodeHelper = require("node_helper");
+
+const https = require("https");
+const digest = require("digest-fetch");
 const ical = require("node-ical");
 const fetch = require("fetch");
-const digest = require("digest-fetch");
-const https = require("https");
+const Log = require("logger");
+const NodeHelper = require("node_helper");
+const CalendarUtils = require("./calendarutils");
 
 /**
  *

commit 4ef030af5f21ada9e1c3017b9f3d414f4063f2c3

Fix import order in serveronly (#3072)
To fix issue caused by #3071

In order to still fulfill the new linter rule `import/order`, I replaced
the alias with the path.

I also see two other approaches, but I opted for the simplest one here
for now.

The other approaches:
1. Also create an alias for `app`.
2. Use new `imports` (like in PR #2934), but let `_moduleAliases`
untouched to stay compatible to 3rd party modules.

**Edit**: Oh, I thought of another option:
- Add `require("module-alias/register");`

serveronly/index.js
@@ -1,5 +1,5 @@
-const Log = require("logger");
 const app = require("../js/app");
+const Log = require("../js/logger");
 
 app.start().then((config) => {
 	const bindAddress = config.address ? config.address : "localhost";