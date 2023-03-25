35e57803d7bc2f4f2a5dda52631c09fae4b0545e
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-03-25 08:16:21 +0100

    Fix import order To fix issue caused by #3071

--- serveronly/index.js
+++ serveronly/index.js
@@ -1,5 +1,5 @@
-const Log = require("logger");
 const app = require("../js/app");
+const Log = require("../js/logger");
 
 app.start().then((config) => {
 	const bindAddress = config.address ? config.address : "localhost";
