2c7beeaaaf111a265975700df9af3374802578c2
Karsten Hassel <hassel@gmx.de>
2023-04-07 19:42:46 +0200

    added test for serveronly (#3086)
    - add test for serveronly, fixes #3076 
    - use template literals in port_variable.js.template

---
+++ tests/e2e/serveronly_spec.js
@@ -0,0 +1,28 @@
+const helpers = require("./helpers/global-setup");
+
+const delay = (time) => {
+	return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+describe("App environment", () => {
+	let serverProcess;
+	beforeAll(async () => {
+		process.env.MM_CONFIG_FILE = "tests/configs/default.js";
+		serverProcess = await require("child_process").spawn("npm", ["run", "server"], { env: process.env, detached: true });
+		// we have to wait until the server is startet
+		await delay(2000);
+	});
+	afterAll(async () => {
+		await process.kill(-serverProcess.pid);
+	});
+
+	it("get request from http://localhost:8080 should return 200", async () => {
+		const res = await helpers.fetch("http://localhost:8080");
+		expect(res.status).toBe(200);
+	});
+
+	it("get request from http://localhost:8080/nothing should return 404", async () => {
+		const res = await helpers.fetch("http://localhost:8080/nothing");
+		expect(res.status).toBe(404);
+	});
+});
