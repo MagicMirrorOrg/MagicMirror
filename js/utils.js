760e4cf4c41478aca04c140fd0743aae0d0169ec
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-01-04 08:04:16 +0100

    Switch to process.versions.electron

--- js/utils.js
+++ js/utils.js
@@ -8,12 +8,6 @@ const colors = require("colors/safe");
 const Log = require("logger");
 const si = require("systeminformation");
 
-try {
-	global.electronVersion = require(`${__dirname}/../node_modules/electron/package.json`).version;
-} catch (error) {
-	global.electronVersion = "not installed";
-}
-
 module.exports = {
 	colors: {
 		warn: colors.yellow,
@@ -28,7 +22,7 @@ module.exports = {
 			let systemDataString = "System information:";
 			systemDataString += `\n ### SYSTEM:   manufacturer: ${staticData["system"]["manufacturer"]}; model: ${staticData["system"]["model"]}; raspberry: ${staticData["system"]["raspberry"]}; virtual: ${staticData["system"]["virtual"]}`;
 			systemDataString += `\n ### OS:       platform: ${staticData["os"]["platform"]}; distro: ${staticData["os"]["distro"]}; release: ${staticData["os"]["release"]}`;
-			systemDataString += `\n ### VERSIONS: MagicMirror: ${global.version}; electron: ${global.electronVersion}; kernel: ${staticData["versions"]["kernel"]}; node: ${staticData["versions"]["node"]}; npm: ${staticData["versions"]["npm"]}; pm2: ${staticData["versions"]["pm2"]}; docker: ${staticData["versions"]["docker"]}`;
+			systemDataString += `\n ### VERSIONS: MagicMirror: ${global.version}; electron: ${process.versions.electron}; kernel: ${staticData["versions"]["kernel"]}; node: ${staticData["versions"]["node"]}; npm: ${staticData["versions"]["npm"]}; pm2: ${staticData["versions"]["pm2"]}; docker: ${staticData["versions"]["docker"]}`;
 			if (typeof staticData["dockerInfo"] !== "undefined") systemDataString += `\n ### DOCKER:   containers: ${staticData["dockerInfo"]["containers"]}; operatingSystem: ${staticData["dockerInfo"]["operatingSystem"]}; osType: ${staticData["versions"]["osType"]}; architecture: ${staticData["versions"]["architecture"]}; serverVersion: ${staticData["versions"]["serverVersion"]}`;
 			Log.info(systemDataString);
 		} catch (e) {
