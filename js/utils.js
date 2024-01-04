6097547c103be1bc89c572e5bd2be50b342967fa
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-01-04 22:38:53 +0100

    Add systeminfo (#3331)
    This is a first attempt to bring additional system information into the
    console (see #3328). It's certainly not yet perfect, but with the PR we
    have a better basis for discussion.
    
    I tried to keep the output small so that we get as much information as
    possible in screenshots.
    
    This is how it looks on my development system.
    
    ```bash
    [03.01.2024 00:50.19.226] [INFO] System information:
     ### SYSTEM:   manufacturer: Notebook; model: N650DU; raspberry: undefined; virtual: false
     ### OS:       platform: linux; distro: Debian GNU/Linux; release: 12
     ### VERSIONS: MagicMirror: 2.27.0-develop; electron: 27.2.0; kernel: 5.10.0-20-amd64; node: 21.1.0; npm: 10.2.4; pm2: 5.3.0; docker: 20.10.24+dfsg1
     ```
     
     Why is it still a draft:
    - [x] I have doubts that utils.js is the right place for the function. What do you think?
    => Update: As long as there is no better idea, it stays there.
    - [x] Instead of working through all wishes you expressed in the issue #3328, I only implemented what was easy to achieve. And wanted to hear what you think about this approach.
    => Update: Some added. Of course, more information could be added later, as soon as experience has been gained in productive use.
    - [x] I don't quite like the introductory line ("The following lines provide information..."). Should I perhaps simply replace it with "System information:"?
    => Update: Changed to "System information:"
     
     [Here](https://github.com/sebhildebrandt/systeminformation#function-reference-and-os-support) you can see what information we could easily add with the systeminformation package.
     
     It would be interesting how the raspberry field is filled on a raspi system and with docker there should be another line, but I can't easily test that now.

--- js/utils.js
+++ js/utils.js
@@ -5,6 +5,8 @@
  * MIT Licensed.
  */
 const colors = require("colors/safe");
+const Log = require("logger");
+const si = require("systeminformation");
 
 module.exports = {
 	colors: {
@@ -12,5 +14,19 @@ module.exports = {
 		error: colors.red,
 		info: colors.blue,
 		pass: colors.green
+	},
+
+	async logSystemInformation  () {
+		try {
+			const staticData = await si.getStaticData();
+			let systemDataString = "System information:";
+			systemDataString += `\n ### SYSTEM:   manufacturer: ${staticData["system"]["manufacturer"]}; model: ${staticData["system"]["model"]}; raspberry: ${staticData["system"]["raspberry"]}; virtual: ${staticData["system"]["virtual"]}`;
+			systemDataString += `\n ### OS:       platform: ${staticData["os"]["platform"]}; distro: ${staticData["os"]["distro"]}; release: ${staticData["os"]["release"]}; arch: ${staticData["os"]["arch"]}; kernel: ${staticData["versions"]["kernel"]}`;
+			systemDataString += `\n ### VERSIONS: electron: ${process.versions.electron}; node: ${staticData["versions"]["node"]}; npm: ${staticData["versions"]["npm"]}; pm2: ${staticData["versions"]["pm2"]}; docker: ${staticData["versions"]["docker"]}`;
+			systemDataString += `\n ### OTHER:    timeZone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}`;
+			Log.info(systemDataString);
+		} catch (e) {
+			Log.error(e);
+		}
 	}
 };
