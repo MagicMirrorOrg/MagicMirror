43bc2d654d9e4913144093322767f5e05875ce10
Ross Younger <crazyscot@gmail.com>
2024-01-29 18:07:20 +1300

    replace embedded non-breaking space with \xa0

--- tests/e2e/modules/weather_current_spec.js
+++ tests/e2e/modules/weather_current_spec.js
@@ -23,7 +23,8 @@ describe("Weather module", () => {
 			});
 
 			it("should render feels like temperature", async () => {
-				await expect(weatherFunc.getText(".weather .normal.medium.feelslike span.dimmed", "93.7  Feels like -5.6°")).resolves.toBe(true);
+				// Template contains &nbsp; which renders as \xa0
+				await expect(weatherFunc.getText(".weather .normal.medium.feelslike span.dimmed", "93.7\xa0 Feels like -5.6°")).resolves.toBe(true);
 			});
 			it("should render humidity next to feels-like", async () => {
 				await expect(weatherFunc.getText(".weather .normal.medium.feelslike span.dimmed .humidity", "93.7")).resolves.toBe(true);
