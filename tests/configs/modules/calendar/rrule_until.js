551e6422090bc8012b8c1d3422b5bfb912813e20
Joshua Kriegshauser <joshuakr@nvidia.com>
2024-03-07 10:43:29 -0800

    unit test

---
+++ tests/configs/modules/calendar/rrule_until.js
@@ -0,0 +1,38 @@
+/* MagicMirror² Test calendar exdate
+ *
+ * By jkriegshauser
+ * MIT Licensed.
+ *
+ * See issue #3250
+ * See tests/electron/modules/calendar_spec.js
+ */
+let config = {
+	timeFormat: 12,
+
+	modules: [
+		{
+			module: "calendar",
+			position: "bottom_bar",
+			config: {
+				hideDuplicates: false,
+				maximumEntries: 100,
+				calendars: [
+					{
+						maximumEntries: 100,
+						maximumNumberOfDays: 1, // Just today
+						url: "http://localhost:8080/tests/mocks/rrule_until.ics"
+					}
+				]
+			}
+		}
+	]
+};
+
+Date.now = () => {
+	return new Date("07 Mar 2024 10:38:00 GMT-07:00").valueOf();
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
