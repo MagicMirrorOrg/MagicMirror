4afdd256d286705e45970d82f13b81de0ab51cdb
sam detweiler <sdetweil@gmail.com>
2024-08-17 14:28:43 -0500

    fix race condition in testcase

--- tests/e2e/custom_module_regions_spec.js
+++ tests/e2e/custom_module_regions_spec.js
@@ -12,21 +12,19 @@ describe("Custom Position of modules", () => {
 	});
 
 	const positions = ["row3_left", "top3_left1"];
-
-	for (let i in positions) {
-		const className = positions[i].replace("_", ".");
-		if (i === 0) {
-			it(`should show text in ${positions[i]}`, async () => {
-				const elem = await helpers.waitForElement(`.${className}`);
-				expect(elem).not.toBeNull();
-				expect(elem.textContent).toContain(`Text in ${positions[i]}`);
-			});
-		}
-		else {
-			it(`should NOT show text in ${positions[i]}`, async () => {
-				const elem = await helpers.waitForElement(`.${className}`, "", 1500);
-				expect(elem).toBeNull();
-			}, 1510);
-		}
-	}
+	let i = 0;
+	const className1 = positions[i].replace("_", ".");
+	let message1 = positions[i];
+	it(`should show text in ${message1}`, async () => {
+		const elem = await helpers.waitForElement(`.${className1}`);
+		expect(elem).not.toBeNull();
+		expect(elem.textContent).toContain(`Text in ${message1}`);
+	});
+	i = 1;
+	const className2 = positions[i].replace("_", ".");
+	let message2 = positions[i];
+	it(`should NOT show text in ${message2}`, async () => {
+		const elem = await helpers.waitForElement(`.${className2}`, "", 1500);
+		expect(elem).toBeNull();
+	}, 1510);
 });
