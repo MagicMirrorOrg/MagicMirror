dc6a2cb7f3cb93645fe1c691ee08188b188cd77f
Karsten Hassel <hassel@gmx.de>
2024-09-01 21:13:29 +0200

    use new env vars MM_MODULES_DIR and MM_CUSTOMCSS_FILE for setting these things from outside (and overriding corresponding config.js properties)

--- js/loader.js
+++ js/loader.js
@@ -11,6 +11,15 @@ const Loader = (function () {
 	/* Private Methods */
 
 	/**
+	 * Retrieve object of env variables.
+	 * @returns {object} with key: values as assembled in js/server_functions.js
+	 */
+	const getEnvVars = async function () {
+		const res = await fetch(`${location.protocol}//${location.host}/env`);
+		return JSON.parse(await res.text());
+	};
+
+	/**
 	 * Loops through all modules and requests start for every module.
 	 */
 	const startModules = async function () {
@@ -58,19 +67,20 @@ const Loader = (function () {
 	 * Generate array with module information including module paths.
 	 * @returns {object[]} Module information.
 	 */
-	const getModuleData = function () {
+	const getModuleData = async function () {
 		const modules = getAllModules();
 		const moduleFiles = [];
+		const envVars = await getEnvVars();
 
 		modules.forEach(function (moduleData, index) {
 			const module = moduleData.module;
 
 			const elements = module.split("/");
 			const moduleName = elements[elements.length - 1];
-			let moduleFolder = `${config.paths.modules}/${module}`;
+			let moduleFolder = `${envVars.modulesDir}/${module}`;
 
 			if (defaultModules.indexOf(moduleName) !== -1) {
-				moduleFolder = `${config.paths.modules}/default/${module}`;
+				moduleFolder = `modules/default/${module}`;
 			}
 
 			if (moduleData.disabled === true) {
@@ -197,7 +207,9 @@ const Loader = (function () {
 		 * Load all modules as defined in the config.
 		 */
 		async loadModules () {
-			let moduleData = getModuleData();
+			let moduleData = await getModuleData();
+			const envVars = await getEnvVars();
+			const customCss = envVars.customCss;
 
 			/**
 			 * @returns {Promise<void>} when all modules are loaded
@@ -212,7 +224,7 @@ const Loader = (function () {
 					// All modules loaded. Load custom.css
 					// This is done after all the modules so we can
 					// overwrite all the defined styles.
-					await loadFile(config.customCss);
+					await loadFile(customCss);
 					// custom.css loaded. Start all modules.
 					await startModules();
 				}
