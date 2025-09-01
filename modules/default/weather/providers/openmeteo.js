0abe55b5e0bdc23fc6017dc8fcdc77e120433892
veeck <gitkraken@veeck.de>
2025-09-01 14:14:58 +0200

    remove accidental changes

--- modules/default/weather/providers/openmeteo.js
+++ modules/default/weather/providers/openmeteo.js
@@ -370,8 +370,6 @@ WeatherProvider.register("openmeteo", {
 		 */
 		const h = moment().hour();
 		const currentWeather = new WeatherObject();
-		Log.info(weather);
-		Log.info(h);
 
 		currentWeather.date = weather.current_weather.time;
 		currentWeather.windSpeed = weather.current_weather.windspeed;
@@ -383,14 +381,12 @@ WeatherProvider.register("openmeteo", {
 		currentWeather.maxTemperature = parseFloat(weather.daily[0].temperature_2m_max);
 		currentWeather.weatherType = this.convertWeatherType(weather.current_weather.weathercode, currentWeather.isDayTime());
 		currentWeather.humidity = parseFloat(weather.hourly[h].relativehumidity_2m);
-		//currentWeather.feelsLikeTemp = parseFloat(weather.hourly[h].apparent_temperature);
+		currentWeather.feelsLikeTemp = parseFloat(weather.hourly[h].apparent_temperature);
 		currentWeather.rain = parseFloat(weather.hourly[h].rain);
 		currentWeather.snow = parseFloat(weather.hourly[h].snowfall * 10);
 		currentWeather.precipitationAmount = parseFloat(weather.hourly[h].precipitation);
 		currentWeather.precipitationProbability = parseFloat(weather.hourly[h].precipitation_probability);
 		currentWeather.uv_index = parseFloat(weather.hourly[h].uv_index);
-
-		Log.info(currentWeather);
 
 		return currentWeather;
 	},
