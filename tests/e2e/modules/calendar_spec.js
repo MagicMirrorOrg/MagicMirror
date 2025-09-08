ed529424f04e95579bafa18cd9cfeaa73b622da0
veeck <gitkraken@veeck.de>
2025-09-08 13:29:40 +0200

    fix tests

--- tests/e2e/modules/calendar_spec.js
+++ tests/e2e/modules/calendar_spec.js
@@ -42,7 +42,7 @@ describe("Calendar module", () => {
 		});
 
 		it("should show the default calendar symbol in each event", async () => {
-			await expect(testElementLength(".calendar .event .fa-calendar-alt", 0, "not")).resolves.toBe(true);
+			await expect(testElementLength(".calendar .event .fa-calendar-days", 0, "not")).resolves.toBe(true);
 		});
 	});
 
