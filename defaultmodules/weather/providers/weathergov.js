3e0d35da8e48bec153ea75eb2e8938417b42711d
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-02-21 23:22:26 +0100

    fix(weathergov): convert wind direction from string to degrees

--- defaultmodules/weather/providers/weathergov.js
+++ defaultmodules/weather/providers/weathergov.js
@@ -306,7 +306,7 @@ class WeatherGovProvider {
 				windSpeed = windSpeedStr.split(" ")[0];
 			}
 			weather.windSpeed = this.#convertWindToMs(parseFloat(windSpeed));
-			weather.windFromDirection = forecast.windDirection;
+			weather.windFromDirection = this.#convertWindDirection(forecast.windDirection);
 			weather.temperature = forecast.temperature;
 			weather.precipitationProbability = forecast.probabilityOfPrecipitation?.value ?? 0;
 			weather.weatherType = this.#convertWeatherType(forecast.shortForecast, forecast.isDaytime);
@@ -315,6 +315,28 @@ class WeatherGovProvider {
 		}
 
 		return hours;
+	}
+
+	#convertWindDirection (direction) {
+		const directions = {
+			N: 0,
+			NNE: 22.5,
+			NE: 45,
+			ENE: 67.5,
+			E: 90,
+			ESE: 112.5,
+			SE: 135,
+			SSE: 157.5,
+			S: 180,
+			SSW: 202.5,
+			SW: 225,
+			WSW: 247.5,
+			W: 270,
+			WNW: 292.5,
+			NW: 315,
+			NNW: 337.5
+		};
+		return directions[direction] ?? null;
 	}
 
 	#convertWindToMs (windSpeedKmh) {
