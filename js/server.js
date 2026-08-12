02034317c98e78889bf832b3361428ce769e5943
Karsten Hassel <hassel@gmx.de>
2026-08-12 10:50:58 +0200

    expand logic of hideConfigSecrets, add more tests (#4229)

--- js/server.js
+++ js/server.js
@@ -93,8 +93,14 @@ function Server (configObj) {
 			app.use("/js", express.static(__dirname));
 
 			if (config.hideConfigSecrets) {
+				const getErrorText = (filename) => {
+					return `<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET /config/${filename}</pre>\n</body>\n</html>`;
+				};
 				app.get("/config/config.env", (req, res) => {
-					res.status(404).send("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"utf-8\">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET /config/config.env</pre>\n</body>\n</html>");
+					res.status(404).send(getErrorText("config.env"));
+				});
+				app.get("/config/config.js", (req, res) => {
+					res.status(404).send(getErrorText("config.js"));
 				});
 			}
 
